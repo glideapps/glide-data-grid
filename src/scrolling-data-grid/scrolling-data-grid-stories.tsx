f4e136cf3a04592fc081a2e82fa5049b22ea2c9c
Alex Corrado <alexander.corrado@gmail.com>
2021-02-24 18:30:09 -0500

    Fix ScrollingDataGrid story
    The values for columns and getCellContent were being recreated
    each time, which caused blitting to always be disabled.
    
    Thanks to Jason for helping track this down and for fixing it.

--- src/scrolling-data-grid/scrolling-data-grid-stories.tsx
+++ src/scrolling-data-grid/scrolling-data-grid-stories.tsx
@@ -6,7 +6,7 @@ import { StoryFnReactReturnType } from "@storybook/react/dist/client/preview/typ
 import { BuilderThemeWrapper } from "../stories/story-utils";
 import GridScroller from "./scrolling-data-grid";
 import { styled } from "../common/styles";
-import { GridCellKind, Rectangle } from "../data-grid/data-grid-types";
+import { GridCell, GridCellKind, Rectangle } from "../data-grid/data-grid-types";
 
 const InnerContainer = styled.div`
     width: 100%;
@@ -51,6 +51,18 @@ export function Simplenotest() {
         setTy(ty);
     }, []);
 
+    const columns = React.useMemo(() => ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"].map(t => ({
+        title: t,
+        width: 122 + (j += 50),
+    })), []);
+
+    const getCellContent = React.useCallback(([col, row]: readonly [number, number]): GridCell => ({
+        kind: GridCellKind.Text,
+        displayData: `${col},${row} Testing things that are way too long`,
+        data: `${col},${row} Testing things that are way too long`,
+        allowOverlay: true,
+    }), []);
+
     return (
         <GridScroller
             rows={10000}
@@ -62,19 +74,11 @@ export function Simplenotest() {
             allowResize={true}
             rowHeight={34}
             onVisibleRegionChanged={onVisibleRegionChanged}
-            columns={["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"].map(t => ({
-                title: t,
-                width: 122 + (j += 50),
-            }))}
-            getCellContent={([col, row]) => ({
-                kind: GridCellKind.Text,
-                displayData: `${col},${row} Testing things that are way too long`,
-                data: `${col},${row} Testing things that are way too long`,
-                allowOverlay: true,
-            })}
-            firstColSticky={false}
-            smoothScrollX={false}
-            smoothScrollY={false}
+            columns={columns}
+            getCellContent={getCellContent}
+            firstColSticky={true}
+            smoothScrollX={true}
+            smoothScrollY={true}
         />
     );
 }
