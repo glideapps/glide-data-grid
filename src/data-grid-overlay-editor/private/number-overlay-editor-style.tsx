aa6123695dae5636fb9b4d5e6b7845c4cf8744fd
Seth Rosetter <seth.rosetter@gmail.com>
2021-06-24 15:18:28 -0700

    Fix theming for numeric input (#52)
    * theme numeric input
    
    * Bump version

--- src/data-grid-overlay-editor/private/number-overlay-editor-style.tsx
+++ src/data-grid-overlay-editor/private/number-overlay-editor-style.tsx
@@ -3,4 +3,8 @@ import { styled } from "../../common/styles";
 export const NumberOverlayEditorStyle = styled.div`
     display: flex;
     color: ${p => p.theme.fgColorDark};
+
+    > input {
+        color: ${p => p.theme.fgColorDark};
+    }
 `;
