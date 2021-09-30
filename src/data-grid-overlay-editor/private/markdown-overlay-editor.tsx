e7f1a2d9112f6e4dd1e8809415e95434e5e661e4
Mark Probst <mark.probst@gmail.com>
2021-09-30 23:00:39 +0000

    begone E

--- src/data-grid-overlay-editor/private/markdown-overlay-editor.tsx
+++ src/data-grid-overlay-editor/private/markdown-overlay-editor.tsx
@@ -6,53 +6,80 @@ import { EditPencil, Checkmark } from "../../common/utils";
 import { GridCell, Rectangle } from "data-grid/data-grid-types";
 
 interface Props {
-    readonly targetRect: Rectangle;
-    readonly markdown: string;
-    readonly onKeyDown: (event: React.KeyboardEvent<HTMLTextAreaElement>) => void;
-    readonly onChange: (ev: React.ChangeEvent<HTMLTextAreaElement>) => void;
-    readonly forceEditMode: boolean;
-    readonly onFinishEditing: (newCell: GridCell | undefined, movement: readonly [-1 | 0 | 1, -1 | 0 | 1]) => void;
+  readonly targetRect: Rectangle;
+  readonly markdown: string;
+  readonly onKeyDown: (event: React.KeyboardEvent<HTMLTextAreaElement>) => void;
+  readonly onChange: (ev: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  readonly forceEditMode: boolean;
+  readonly onFinishEditing: (
+    newCell: GridCell | undefined,
+    movement: readonly [-1 | 0 | 1, -1 | 0 | 1]
+  ) => void;
 
-    readonly readonly: boolean;
+  readonly readonly: boolean;
 
-    createNode?: (content: string) => DocumentFragment;
+  createNode?: (content: string) => DocumentFragment;
 }
 
-const MarkdownOverlayEditor: React.FunctionComponent<Props> = p => {
-    const { markdown, onChange, onKeyDown, forceEditMode, createNode, targetRect, readonly, onFinishEditing } = p;
+const MarkdownOverlayEditor: React.FunctionComponent<Props> = (p) => {
+  const {
+    markdown,
+    onChange,
+    onKeyDown,
+    forceEditMode,
+    createNode,
+    targetRect,
+    readonly,
+    onFinishEditing,
+  } = p;
 
-    const [editMode, setEditMode] = React.useState<boolean>(markdown === "" || forceEditMode);
-    const onEditClick = React.useCallback(() => {
-        setEditMode(e => !e);
-    }, []);
-    const addLeftPad = markdown ? "ml-6" : "";
+  const [editMode, setEditMode] = React.useState<boolean>(
+    markdown === "" || forceEditMode
+  );
+  const onEditClick = React.useCallback(() => {
+    setEditMode((e) => !e);
+  }, []);
+  const addLeftPad = markdown ? "ml-6" : "";
 
-    if (editMode) {
-        return (
-            <MarkdownOverlayEditorStyle targetRect={targetRect}>
-                <GrowingEntry autoFocus={true} onKeyDown={onKeyDown} value={markdown} onChange={onChange} />
-                <div
-                    className={`edit-icon checkmark-hover ${addLeftPad}`}
-                    onClick={e => {
-                        onEditClick();
-                        onFinishEditing(undefined, [0, 1]);
-                    }}>
-                    <Checkmark />
-                </div>
-            </MarkdownOverlayEditorStyle>
-        );
-    }
+  if (editMode) {
     return (
-        <MarkdownOverlayEditorStyle targetRect={targetRect}>
-            <MarkdownDiv contents={markdown} createNode={createNode} />
-            {!readonly && (
-                <div className={`edit-icon edit-hover ${addLeftPad}`} onClick={onEditClick}>
-                    <EditPencil />
-                </div>
-            )}
-            <textarea className="md-edit-textarea" autoFocus={true} onKeyDown={onKeyDown} />
-        </MarkdownOverlayEditorStyle>
+      <MarkdownOverlayEditorStyle targetRect={targetRect}>
+        <GrowingEntry
+          autoFocus={true}
+          onKeyDown={onKeyDown}
+          value={markdown}
+          onChange={onChange}
+        />
+        <div
+          className={`edit-icon checkmark-hover ${addLeftPad}`}
+          onClick={() => {
+            onEditClick();
+            onFinishEditing(undefined, [0, 1]);
+          }}
+        >
+          <Checkmark />
+        </div>
+      </MarkdownOverlayEditorStyle>
     );
+  }
+  return (
+    <MarkdownOverlayEditorStyle targetRect={targetRect}>
+      <MarkdownDiv contents={markdown} createNode={createNode} />
+      {!readonly && (
+        <div
+          className={`edit-icon edit-hover ${addLeftPad}`}
+          onClick={onEditClick}
+        >
+          <EditPencil />
+        </div>
+      )}
+      <textarea
+        className="md-edit-textarea"
+        autoFocus={true}
+        onKeyDown={onKeyDown}
+      />
+    </MarkdownOverlayEditorStyle>
+  );
 };
 
 export default MarkdownOverlayEditor;
