6183953badcc96193ccb2b6d181fd9f6044f86bc
ivoelbert <ivo.elbert1@gmail.com>
2021-10-15 11:48:04 -0300

    Optimize animation amnager

--- src/data-grid/animation-manager.ts
+++ src/data-grid/animation-manager.ts
@@ -10,60 +10,104 @@ const hoverTime = 120;
 export class AnimationManager {
     constructor(private callback: StepCallback) {}
 
-    private state: StateItem[] = [];
-    private hoveredItem: Item | undefined;
-    private lastAnimationTime = 0;
+    private currentHoveredItem: StateItem | undefined = undefined;
+    private leavingItems: StateItem[] = [];
 
-    private areSame(left: Item | undefined, right: Item | undefined) {
+    private lastAnimationTime: number | undefined;
+
+    private areSameItems = (left: Item | undefined, right: Item | undefined) => {
         return left?.[0] === right?.[0] && left?.[1] === right?.[1];
-    }
+    };
 
-    private indexOf(item: Item): number {
-        return this.state.findIndex(x => x.item[0] === item[0] && x.item[1] === item[1]);
-    }
+    private addToLeavingItems = (item: StateItem) => {
+        const isAlreadyLeaving = this.leavingItems.find(i => this.areSameItems(i.item, item.item)) !== undefined;
+
+        if (isAlreadyLeaving) {
+            return;
+        }
+
+        this.leavingItems.push(item);
+    };
+
+    /**
+     * @returns the hover amount of the item, if it was leaving (0 if not).
+     */
+    private removeFromLeavingItems = (item: Item): number => {
+        const leavingItem = this.leavingItems.find(e => this.areSameItems(e.item, item));
+
+        this.leavingItems = this.leavingItems.filter(i => i !== leavingItem);
+
+        return leavingItem?.hoverAmount ?? 0;
+    };
+
+    private cleanUpLeavingElements = () => {
+        this.leavingItems = this.leavingItems.filter(i => i.hoverAmount > 0);
+    };
+
+    private shouldStep = () => {
+        const hasLeavingItems = this.leavingItems.length > 0;
+        const currentHoveredIsAnimating =
+            this.currentHoveredItem !== undefined && this.currentHoveredItem.hoverAmount < 1;
+
+        return hasLeavingItems || currentHoveredIsAnimating;
+    };
+
+    private getAnimatingItems = () => {
+        if (this.currentHoveredItem !== undefined) {
+            return [...this.leavingItems, this.currentHoveredItem];
+        }
+        return this.leavingItems;
+    };
 
     private step = (timestamp: number): void => {
-        if (this.lastAnimationTime === 0) {
+        if (this.lastAnimationTime === undefined) {
             this.lastAnimationTime = timestamp;
         } else {
             const step = timestamp - this.lastAnimationTime;
             const delta = step / hoverTime;
 
-            for (const s of this.state) {
-                const scalar = this.areSame(this.hoveredItem, s.item) ? 1 : -1;
-                s.hoverAmount = clamp(s.hoverAmount + delta * scalar, 0, 1);
+            for (const item of this.leavingItems) {
+                item.hoverAmount = clamp(item.hoverAmount - delta, 0, 1);
             }
 
-            this.callback(this.state);
+            if (this.currentHoveredItem !== undefined) {
+                this.currentHoveredItem.hoverAmount = clamp(this.currentHoveredItem.hoverAmount + delta, 0, 1);
+            }
 
-            this.state = this.state.filter(s => s.hoverAmount > 0);
+            this.callback(this.getAnimatingItems());
+
+            this.cleanUpLeavingElements();
         }
-        if (this.state.length > 0) {
-            window.requestAnimationFrame(this.step);
+
+        if (this.shouldStep()) {
             this.lastAnimationTime = timestamp;
+            window.requestAnimationFrame(this.step);
         } else {
-            this.lastAnimationTime = 0;
+            this.lastAnimationTime = undefined;
         }
     };
 
-    private startAnimationIfNeeded() {
-        if (this.lastAnimationTime !== 0 && this.state.length > 0) return;
-
-        window.requestAnimationFrame(this.step);
-    }
-
-    public setHovered(item: Item | undefined): void {
-        this.hoveredItem = item;
-        if (item !== undefined) {
-            const index = this.indexOf(item);
-            if (index === -1) {
-                this.state.push({
-                    item,
-                    hoverAmount: 0,
-                });
-            }
+    public setHovered = (item: Item | undefined): void => {
+        if (this.areSameItems(this.currentHoveredItem?.item, item)) {
+            return;
         }
 
-        this.startAnimationIfNeeded();
-    }
+        if (this.currentHoveredItem !== undefined) {
+            this.addToLeavingItems(this.currentHoveredItem);
+        }
+
+        if (item !== undefined) {
+            const hoverAmount = this.removeFromLeavingItems(item);
+            this.currentHoveredItem = {
+                item,
+                hoverAmount,
+            };
+        } else {
+            this.currentHoveredItem = undefined;
+        }
+
+        if (this.lastAnimationTime === undefined) {
+            window.requestAnimationFrame(this.step);
+        }
+    };
 }
