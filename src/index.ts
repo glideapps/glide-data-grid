5c23c6364f8a3ce3cac01f007f671d6db32b5b95
Jason Smith <jason@heyglide.com>
2021-10-30 04:14:39 +0000

    Cleanup index.js

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import { DataEditor } from "./data-editor/data-editor";
 
-export {
-    default as ImageOverlayEditor,
-    OverlayImageEditorProps,
-} from "./data-grid-overlay-editor/private/image-overlay-editor";
+export type { OverlayImageEditorProps } from "./data-grid-overlay-editor/private/image-overlay-editor";
+export { default as ImageOverlayEditor } from "./data-grid-overlay-editor/private/image-overlay-editor";
 
-export { default as MarkdownDiv, MarkdownDivProps } from "./markdown-div/markdown-div";
+export type { MarkdownDivProps } from "./markdown-div/markdown-div";
+export { default as MarkdownDiv } from "./markdown-div/markdown-div";
 export * from "./data-editor/data-editor";
 export * from "./data-grid/data-grid-types";
 export { default as DataEditorContainer } from "./data-editor-container/data-grid-container";
-export { Theme } from "./common/styles";
+export type { Theme } from "./common/styles";
 export default DataEditor;
