e52d2c52a4fc4b36a54387de198cb49375e6b15f
Jason Smith <jason@heyglide.com>
2021-11-02 15:40:40 +0000

    Bootstrap on publish

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,18 +5,23 @@ set -e
 VERSION=`jq '.version' package.json`
 npm install
 
+function update {
+    echo $1 $2
+    jq "$1" $2 > $2.tmp
+    mv $2.tmp $2
+}
+
 pushd core
 npm ci
-jq ".version = $VERSION" package.json > package.json.tmp
-mv package.json.tmp package.json
+update ".version = $VERSION" package.json
 popd
 
 
 pushd cells
-jq ".dependencies.\"@glideapps/glide-data-grid\" = $VERSION" package.json > package.json.tmp
-jq ".version = $VERSION" package.json.tmp > package.json
-rm package.json.tmp
+update ".dependencies.\"@glideapps/glide-data-grid\" = \"3.0.2\"" package.json
 npm ci
+update ".dependencies.\"@glideapps/glide-data-grid\" = $VERSION" package.json
+update ".version = $VERSION" package.json
 rm -rf node_modules/@glideapps/glide-data-grid
 ln -s ../../../core node_modules/@glideapps/glide-data-grid
 popd
