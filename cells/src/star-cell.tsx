1b4cc392423ab5621371076e6212eeafd0b98842
Jason Smith <jason@heyglide.com>
2021-11-03 22:31:55 +0000

    Improve rating picker

--- cells/src/star-cell.tsx
+++ cells/src/star-cell.tsx
@@ -1,6 +1,7 @@
 import { CustomCell } from "@glideapps/glide-data-grid";
 import { CustomCellRenderer } from "./types";
 import * as React from "react";
+import styled from "styled-components";
 
 interface StarCellProps {
     readonly kind: "star-cell";
@@ -39,6 +40,39 @@ function pathStar(ctx: CanvasRenderingContext2D, center: readonly [number, numbe
     ctx.closePath();
 }
 
+const StarSVG = () => (
+    <svg width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M47.1468 13.7811C48.0449 11.0172 51.9551 11.0172 52.8532 13.7812L60.5522 37.4762C60.9538 38.7123 62.1056 39.5491 63.4053 39.5491H88.3198C91.226 39.5491 92.4343 43.268 90.0831 44.9762L69.9269 59.6205C68.8755 60.3845 68.4355 61.7386 68.8371 62.9746L76.5361 86.6697C77.4342 89.4336 74.2707 91.732 71.9196 90.0238L51.7634 75.3794C50.7119 74.6155 49.2881 74.6155 48.2366 75.3795L28.0804 90.0238C25.7293 91.732 22.5659 89.4336 23.4639 86.6697L31.1629 62.9746C31.5645 61.7386 31.1245 60.3845 30.0731 59.6205L9.91686 44.9762C7.56572 43.268 8.77405 39.5491 11.6802 39.5491H36.5947C37.8944 39.5491 39.0462 38.7123 39.4478 37.4762L47.1468 13.7811Z"
+            fill="currentColor"
+        />
+    </svg>
+);
+
+const EditorWrap = styled.div`
+    .active {
+        color: ${p => p.theme.textDark};
+    }
+
+    display: flex;
+    align-items: center;
+    padding: 6px 0;
+    color: ${p => p.theme.textLight};
+
+    > * {
+        position: relative;
+        width: 16px;
+        height: 16px;
+        cursor: pointer;
+        margin-right: 2px;
+
+        svg {
+            width: 100%;
+            height: 100%;
+        }
+    }
+`;
+
 const renderer: CustomCellRenderer<StarCell> = {
     isMatch: (cell: CustomCell): cell is StarCell => (cell.data as any).kind === "star-cell",
     draw: (ctx, cell, theme, rect, hoverAmount) => {
@@ -61,20 +95,24 @@ const renderer: CustomCellRenderer<StarCell> = {
     provideEditor: () => {
         // eslint-disable-next-line react/display-name
         return p => (
-            <input
-                value={p.value.data.rating}
-                onChange={e => {
-                    const newNumber = Number.parseInt(e.target.value) ?? p.value.data.rating;
-                    p.onChange({
-                        ...p.value,
-                        data: {
-                            ...p.value.data,
-                            rating: newNumber,
-                        },
-                        copyData: newNumber.toString(),
-                    });
-                }}
-            />
+            <EditorWrap>
+                {[0, 1, 2, 3, 4].map(index => (
+                    <div
+                        key={index}
+                        className={p.value.data.rating < index + 1 ? "inactive" : "active"}
+                        onClick={() =>
+                            p.onChange({
+                                ...p.value,
+                                data: {
+                                    ...p.value.data,
+                                    rating: index + 1,
+                                },
+                            })
+                        }>
+                        <StarSVG />
+                    </div>
+                ))}
+            </EditorWrap>
         );
     },
 };
