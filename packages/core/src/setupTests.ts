09eb31271224cb0a86a6843d72c8b88ef918819b
Jason Smith <jason@heyglide.com>
2021-12-23 02:22:47 +0000

    Bad example of use-data-source

--- packages/core/src/setupTests.ts
+++ packages/core/src/setupTests.ts
@@ -1,7 +1,4 @@
 import "@testing-library/jest-dom";
-import Enzyme from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
 import "jest-canvas-mock";
 
-Enzyme.configure({ adapter: new Adapter() });
 Image.prototype.decode = () => new Promise(resolve => window.setTimeout(resolve, 10));
