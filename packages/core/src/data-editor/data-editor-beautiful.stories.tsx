7308b315e1d4bdf5af08f2af6b2626bad45b2bd5
Peter <peter@config.com>
2022-02-10 15:39:51 -0800

    Add Padding storybook story

--- packages/core/src/data-editor/data-editor-beautiful.stories.tsx
+++ packages/core/src/data-editor/data-editor-beautiful.stories.tsx
@@ -571,6 +571,63 @@ export const Overscroll: React.VFC<OverscrollProps> = p => {
     },
 };
 
+
+interface PaddingProps {
+    paddingRight: number;
+    paddingBottom: number;
+}
+
+export const Padding: React.VFC<PaddingProps> = p => {
+    const { paddingRight, paddingBottom } = p;
+    const { cols, getCellContent } = useMockDataGenerator(20);
+
+    return (
+        <BeautifulWrapper
+            title="Padding"
+            description={
+                <>
+                    <Description>
+                        You can add padding at the ends of the grid by setting the{" "}
+                        <PropName>paddingRight</PropName> and <PropName>paddingBottom</PropName> props
+                    </Description>
+                </>
+            }>
+            <DataEditor
+                {...defaultProps}
+                getCellContent={getCellContent}
+                columns={cols}
+                experimental={{ paddingRight, paddingBottom }}
+                rows={50}
+            />
+        </BeautifulWrapper>
+    );
+};
+(Padding as any).argTypes = {
+    paddingRight: {
+        control: {
+            type: "range",
+            min: 0,
+            max: 600,
+        },
+    },
+    paddingBottom: {
+        control: {
+            type: "range",
+            min: 0,
+            max: 600,
+        },
+    },
+};
+(Padding as any).args = {
+    paddingRight: 200,
+    paddingBottom: 200,
+};
+(Padding as any).parameters = {
+    options: {
+        showPanel: true,
+    },
+};
+
 function clearCell(cell: GridCell): GridCell {
     switch (cell.kind) {
         case GridCellKind.Boolean: {
