f358c6c1b472578bec6e3edef15c69ece90c99a7
Jason <jason@heyglide.com>
2022-03-14 23:16:06 -0700

    Export default theme

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,16 +1,18 @@
 import { DataEditor } from "./data-editor/data-editor";
 
 export type { OverlayImageEditorProps } from "./data-grid-overlay-editor/private/image-overlay-editor";
-export { default as ImageOverlayEditor } from "./data-grid-overlay-editor/private/image-overlay-editor";
-
 export type { MarkdownDivProps } from "./markdown-div/markdown-div";
-export { default as MarkdownDiv } from "./markdown-div/markdown-div";
+export type { SpriteMap } from "./data-grid/data-grid-sprites";
+export type { Theme } from "./common/styles";
+
 export * from "./data-editor/data-editor";
 export * from "./data-grid/data-grid-types";
-export type { SpriteMap } from "./data-grid/data-grid-sprites";
+export { default as ImageOverlayEditor } from "./data-grid-overlay-editor/private/image-overlay-editor";
+export { default as MarkdownDiv } from "./markdown-div/markdown-div";
+export { default as TextCellEntry } from "./growing-entry/growing-entry";
 export { default as DataEditorContainer } from "./data-editor-container/data-grid-container";
-export type { Theme } from "./common/styles";
 export { parseToRgba } from "./data-grid/color-parser";
 export { measureTextCached } from "./data-grid/data-grid-lib";
-export { default as TextCellEntry } from "./growing-entry/growing-entry";
+export { getDataEditorTheme as getDefaultTheme } from "./common/styles";
+
 export default DataEditor;
