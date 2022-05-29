b204c754f571db94c97702a3839c5fe976077a32
Jason Smith <jason@heyglide.com>
2022-05-29 08:41:31 -0700

    Missing tests

--- packages/core/src/common/common.test.ts
+++ packages/core/src/common/common.test.ts
@@ -1,4 +1,4 @@
-import { maybe } from "./support";
+import { deepEqual, maybe } from "./support";
 import { describe, test, expect } from "jest-without-globals";
 
 describe("maybe", () => {
@@ -14,5 +14,15 @@ describe("maybe", () => {
         }, "fail");
 
         expect(result).toBe("fail");
+    });
+});
+
+describe("deepEqual", () => {
+    test("Deep equal smoke tests", () => {
+        expect(deepEqual([1, 2, 3], [1, 2, 3])).toBe(true);
+        expect(deepEqual([1, 2, 3], [1, 23])).toBe(false);
+        expect(deepEqual({ a: "b", c: "d" }, { a: "b", c: "d" })).toBe(true);
+        expect(deepEqual({ a: "b", c: "d" }, [1, 2, 3])).toBe(false);
+        expect(deepEqual({ a: "b", c: { d: "e" } }, { a: "b", c: { d: "e" } })).toBe(true);
     });
 });
