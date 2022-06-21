f4368488b09e8092c19e01092de44a52b0f41b4a
Peter <peter@config.com>
2022-06-21 15:14:15 -0700

    Add AppendRowHandle story

--- packages/core/src/data-editor/stories/data-editor-beautiful.stories.tsx
+++ packages/core/src/data-editor/stories/data-editor-beautiful.stories.tsx
@@ -520,6 +520,62 @@ export const AddDataToMiddle: React.FC<AddDataToMiddleProps> = p => {
     },
 };
 
+export const AppendRowHandle: React.VFC = () => {
+    const { cols, getCellContent, setCellValueRaw, setCellValue } = useMockDataGenerator(60, false);
+
+    const [numRows, setNumRows] = React.useState(50);
+
+    const ref = React.useRef<DataEditorRef>(null);
+
+    const onClick = React.useCallback(() => {
+      ref.current?.appendRow(4)
+    }, [ref]);
+
+    const onRowAppended = React.useCallback(() => {
+        const newRow = numRows;
+        for (let c = 0; c < 6; c++) {
+            const cell = getCellContent([c, newRow]);
+            setCellValueRaw([c, newRow], clearCell(cell));
+        }
+        setNumRows(cv => cv + 1);
+    }, [getCellContent, numRows, setCellValueRaw]);
+
+    return (
+        <BeautifulWrapper
+            title="appendRow Ref"
+            description={
+                <>
+                    <Description>Adding data can also be triggered from outside of <PropName>DataEditor</PropName></Description>
+                    <MoreInfo>
+                        By calling <PropName>appendRow</PropName> on a <PropName>ref</PropName> to your grid, you can trigger
+                        the append elsewhere, like this <KeyName onClick={onClick}>Append</KeyName> button
+                    </MoreInfo>
+                </>
+            }>
+            <DataEditor
+                {...defaultProps}
+                ref={ref}
+                getCellContent={getCellContent}
+                columns={cols}
+                rowMarkers={"both"}
+                onCellEdited={setCellValue}
+                trailingRowOptions={{
+                    hint: "New row...",
+                    sticky: true,
+                    tint: true,
+                }}
+                rows={numRows}
+                onRowAppended={onRowAppended}
+            />
+        </BeautifulWrapper>
+    );
+};
+(AppendRowHandle as any).parameters = {
+    options: {
+        showPanel: false,
+    },
+};
+
 export const SmallEditableGrid = () => {
     const { cols, getCellContent, setCellValue } = useMockDataGenerator(6, false);
 
