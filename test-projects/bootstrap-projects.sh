ce3beb88e147f4dc8b03b8c21bc7995bff0eca22
Jason <jason@heyglide.com>
2022-06-26 22:06:27 -0700

    Fix cra examples too

--- test-projects/bootstrap-projects.sh
+++ test-projects/bootstrap-projects.sh
@@ -8,6 +8,7 @@ do
     npm ci
     rm -rf node_modules/@glideapps/glide-data-grid/dist
     cp -r ../../packages/core/dist node_modules/@glideapps/glide-data-grid/dist
+    cp ../../packages/core/package.json node_modules/@glideapps/glide-data-grid
     npm run build
     popd
 done
