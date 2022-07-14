252050bcb1a304f7837f697c1a146959a494e4d8
Jason <jason@heyglide.com>
2022-07-14 14:05:36 -0700

    Add delay to tooltip example

--- packages/core/src/data-editor/stories/data-editor-beautiful.stories.tsx
+++ packages/core/src/data-editor/stories/data-editor-beautiful.stories.tsx
@@ -2869,24 +2869,34 @@ export const Tooltips: React.VFC = () => {
 
     const [tooltip, setTooltip] = React.useState<{ val: string; bounds: IBounds } | undefined>();
 
+    const timeoutRef = React.useRef(0);
+
     const onItemHovered = React.useCallback((args: GridMouseEventArgs) => {
         if (args.kind === "cell") {
-            setTooltip({
-                val: `Tooltip for ${args.location[0]}, ${args.location[1]}`,
-                bounds: {
-                    // translate to react-laag types
-                    left: args.bounds.x,
-                    top: args.bounds.y,
-                    width: args.bounds.width,
-                    height: args.bounds.height,
-                    right: args.bounds.x + args.bounds.width,
-                    bottom: args.bounds.y + args.bounds.height,
-                },
-            });
+            window.clearTimeout(timeoutRef.current);
+            setTooltip(undefined);
+            timeoutRef.current = window.setTimeout(() => {
+                setTooltip({
+                    val: `Tooltip for ${args.location[0]}, ${args.location[1]}`,
+                    bounds: {
+                        // translate to react-laag types
+                        left: args.bounds.x,
+                        top: args.bounds.y,
+                        width: args.bounds.width,
+                        height: args.bounds.height,
+                        right: args.bounds.x + args.bounds.width,
+                        bottom: args.bounds.y + args.bounds.height,
+                    },
+                });
+            }, 1000);
         } else {
+            window.clearTimeout(timeoutRef.current);
+            timeoutRef.current = 0;
             setTooltip(undefined);
         }
     }, []);
+
+    React.useEffect(() => () => window.clearTimeout(timeoutRef.current), []);
 
     const isOpen = tooltip !== undefined;
     const { renderLayer, layerProps } = useLayer({
