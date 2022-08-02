13b7ed984d2437d5fc05819d66b2fbe29f2d5f7e
Jason Smith <jason@heyglide.com>
2022-08-02 09:06:29 -0700

    Imports instead of globals

--- packages/core/src/data-editor/use-autoscroll.test.tsx
+++ packages/core/src/data-editor/use-autoscroll.test.tsx
@@ -1,4 +1,5 @@
 import { renderHook } from "@testing-library/react-hooks";
+import { describe, it, expect } from "jest-without-globals";
 import { useAutoscroll } from "./use-autoscroll";
 
 describe("use-auto-scroll", () => {
