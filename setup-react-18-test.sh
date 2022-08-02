e4017105a0297c1c0c99beaea0017dfb48400a25
Jason Smith <jason@heyglide.com>
2022-08-02 09:24:56 -0700

    Fix react 18 testing

--- setup-react-18-test.sh
+++ setup-react-18-test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-npm i -D react@latest react-dom@latest @testing-library/jest-dom@latest @testing-library/react@latest @testing-library/react-hooks@latest @testing-library/user-event@latest react-test-renderer@latest
+npm i -D react@latest react-dom@latest @testing-library/jest-dom@latest @testing-library/react@latest @testing-library/react-hooks@latest @testing-library/user-event@14.3.0 react-test-renderer@latest
