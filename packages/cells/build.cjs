5b16bf66319d7b970ed02baab563879a1d449fea
Jason Smith <jason@heyglide.com>
2022-08-29 13:37:08 -0700

    Fix deps culling

--- packages/cells/build.cjs
+++ packages/cells/build.cjs
@@ -1,6 +1,7 @@
 const { build } = require("esbuild");
+const glob = require("glob");
 const linaria = require("@linaria/esbuild/lib/index").default;
-const { dependencies } = require("./package.json");
+const { dependencies, peerDependencies } = require("./package.json");
 const fs = require("fs");
 
 const shared = {
@@ -13,7 +14,7 @@ const shared = {
             sourceMap: false,
         }),
     ],
-    external: Object.keys(dependencies),
+    external: Object.keys(dependencies).concat(["react", "react-dom", "styled-components"]),
 };
 
 async function f() {
@@ -30,8 +31,14 @@ async function f() {
         format: "esm",
     });
 
-    fs.copyFileSync("dist/js/index.css", "dist/index.css");
-    fs.rmSync("dist/js/index.css");
+    fs.copyFileSync("dist/cjs/index.css", "dist/index.css");
+    glob("dist/js/*.css", {}, function (_er, files) {
+        // files is an array of filenames.
+        // If the `nonull` option is set, and nothing
+        // was found, then files is ["**/*.js"]
+        // er is an error object or null.
+        files.forEach(f => fs.rmSync(f));
+    });
     fs.rmSync("dist/cjs/index.css");
 }
 
