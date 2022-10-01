737b8f00b06992845c9bb2d5f60200b36e12af2d
Jason <jason@heyglide.com>
2022-09-30 21:54:13 -0700

    Update deps

--- packages/core/build.cjs
+++ packages/core/build.cjs
@@ -32,8 +32,11 @@ async function f() {
     });
 
     fs.copyFileSync("dist/js/index.css", "dist/index.css");
+    fs.copyFileSync("dist/js/index.css.map", "dist/index.css.map");
     fs.rmSync("dist/js/index.css");
     fs.rmSync("dist/cjs/index.css");
+    fs.rmSync("dist/js/index.css.map");
+    fs.rmSync("dist/cjs/index.css.map");
 }
 
 f();
