2f69a40368804fb1351556055c5ed04e4bc90d7e
willhuang1997 <willhuang1997@gmail.com>
2023-01-31 18:27:57 -0800

    DatePickerCell Implementation with prettier

--- packages/cells/src/cells/date-picker-cell.tsx
+++ packages/cells/src/cells/date-picker-cell.tsx
@@ -1,14 +1,86 @@
-import * as React from "react";
-import { CustomCell, CustomRenderer, drawTextCell, GridCellKind } from "@glideapps/glide-data-grid";
+import React from "react";
+import {
+    CustomCell,
+    CustomRenderer,
+    drawTextCell,
+    GridCellKind,
+    ProvideEditorCallback,
+} from "@glideapps/glide-data-grid";
 
 interface DatePickerCellProps {
     readonly kind: "date-picker-cell";
     readonly date: Date | undefined;
     readonly displayDate: string;
-    readonly format: "date" | "datetime-local";
+    readonly format: "date" | "time" | "datetime-local";
 }
 
+export const formatValueForHTMLInput = (format: "date" | "time" | "datetime-local", date: Date): string => {
+    if (format === "date") {
+        return date.toISOString().split("T")[0];
+    }
+    if (format === "time") {
+        return date.toISOString().split("T")[1].replace("Z", "");
+    }
+    if (format === "datetime-local") {
+        return date.toISOString().replace("Z", "");
+    }
+    return "";
+};
+
 export type DatePickerCell = CustomCell<DatePickerCellProps>;
+
+const Editor: ReturnType<ProvideEditorCallback<DatePickerCell>> = cell => {
+    const cellData = cell.value.data;
+    const { date, displayDate, format } = cellData;
+    const cellDataDate =
+        cellData.date ??
+        // now as date with adjusted timezone to allow for displayDate to be correct
+        new Date(new Date().getTime() - new Date().getTimezoneOffset() * 60000);
+    const value = formatValueForHTMLInput(format, cellDataDate);
+
+    return (
+        <input
+            required
+            style={{ minHeight: 26, border: "none", outline: "none" }}
+            type={format}
+            value={value}
+            autoFocus={true}
+            onChange={event => {
+                // handle when clear is clicked and value has been wiped
+                if (event.target.value === "") {
+                    try {
+                        cell.onChange({
+                            ...cell.value,
+                            data: {
+                                ...cell.value.data,
+                                // attempt to reset to cached date
+                                date: date !== undefined ? date : new Date(displayDate),
+                            },
+                        });
+                    } catch (error) {
+                        cell.onChange({
+                            ...cell.value,
+                            data: {
+                                ...cell.value.data,
+                                displayDate: String(error),
+                            },
+                        });
+                    }
+                    return;
+                }
+                cell.onChange({
+                    ...cell.value,
+                    data: {
+                        ...cell.value.data,
+                        // use valueAsNumber because valueAsDate is null for "datetime-local"
+                        // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/datetime-local#technical_summary
+                        date: new Date(event.target.valueAsNumber) ?? cellDataDate,
+                    },
+                });
+            }}
+        />
+    );
+};
 
 const renderer: CustomRenderer<DatePickerCell> = {
     kind: GridCellKind.Custom,
@@ -18,36 +90,9 @@ const renderer: CustomRenderer<DatePickerCell> = {
         drawTextCell(args, displayDate, cell.contentAlign);
         return true;
     },
-    // eslint-disable-next-line react/display-name
-    provideEditor: () => p => {
-        const cellData = p.value.data;
-        const { format, date } = cellData;
-
-        let val = "";
-        if (date !== undefined) {
-            val = date.toISOString();
-            if (format === "date") {
-                val = val.split("T")[0];
-            }
-        }
-        return (
-            <input
-                style={{ minHeight: 26, border: "none", outline: "none" }}
-                type={format}
-                autoFocus={true}
-                value={val}
-                onChange={e => {
-                    p.onChange({
-                        ...p.value,
-                        data: {
-                            ...p.value.data,
-                            date: e.target.valueAsDate ?? undefined,
-                        },
-                    });
-                }}
-            />
-        );
-    },
+    provideEditor: () => ({
+        editor: Editor,
+    }),
     onPaste: (v, d) => {
         let newDate: Date | undefined;
         try {
@@ -55,7 +100,6 @@ const renderer: CustomRenderer<DatePickerCell> = {
         } catch {
             /* do nothing */
         }
-
         return {
             ...d,
             date: Number.isNaN(newDate) ? undefined : newDate,
