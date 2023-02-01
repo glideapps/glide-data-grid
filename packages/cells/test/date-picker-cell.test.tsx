8b0f5d8e4efb4189f17608a891d95243c8edaadb
willhuang1997 <willhuang1997@gmail.com>
2023-02-01 00:29:12 -0800

    Add todos for myself

--- packages/cells/test/date-picker-cell.test.tsx
+++ packages/cells/test/date-picker-cell.test.tsx
@@ -1,14 +1,17 @@
-import { DateKind, formatValueForHTMLInput } from "../src/cells/date-picker-cell"
+import { DateKind, formatValueForHTMLInput } from "../src/cells/date-picker-cell";
 
-describe(("formatValueForHTMLInput"), () => {
+describe("formatValueForHTMLInput", () => {
     it.each([
         ["date", new Date("1970-01-01T00:00:00.100Z"), "1970-01-01"],
         ["datetime-local", new Date("1970-01-01T00:00:00.100Z"), "1970-01-01T00:00:00.100"],
         ["time", new Date("1970-01-01T00:00:00.100Z"), "00:00:00.100"],
     ])("check %p format and %p date leads correct format: {%p}", (format: string, date: Date, valueForHTML: string) => {
-        expect(formatValueForHTMLInput(format as DateKind, date)).toStrictEqual(valueForHTML)
-    })
-    it('throws an error when a weird value is passed', () => {
-        expect(() => formatValueForHTMLInput("weird" as DateKind, new Date())).toThrow("Hell froze over")
-    })
-})
+        expect(formatValueForHTMLInput(format as DateKind, date)).toStrictEqual(valueForHTML);
+    });
+    it("throws an error when a weird value is passed", () => {
+        expect(() => formatValueForHTMLInput("weird" as DateKind, new Date())).toThrow("Hell froze over");
+    });
+});
+
+// TODO(willhuang1997): Add tests for onChange in input html
+// TODO(willhuang1997): Add tests for onPaste
