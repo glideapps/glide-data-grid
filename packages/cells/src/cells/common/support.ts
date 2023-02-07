497f071809af0116f7a1d49c4a66cf7a81eb4dba
lukasmasuch <lukas.masuch@gmail.com>
2023-02-07 22:38:09 +0100

    Delete support ts

--- packages/cells/src/cells/common/support.ts
+++
@@ -1,7 +0,0 @@
-function panic(message: string = "This should not happen"): never {
-    throw new Error(message);
-}
-
-export function assertNever(_never: never): never {
-    return panic("Hell froze over");
-}
