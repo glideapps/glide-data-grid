8e88f2c266b2098c8ccca6e71f62b8b38b6a136c
lukasmasuch <lukas.masuch@gmail.com>
2023-02-08 21:19:54 +0100

    Remove theme

--- packages/cells/src/cell.stories.tsx
+++ packages/cells/src/cell.stories.tsx
@@ -464,41 +464,6 @@ export const CustomCells: React.VFC = () => {
                         width: 120,
                     },
                 ]}
-                theme={{
-                    accentColor: "#8c96ff",
-                    accentLight: "rgba(202, 206, 255, 0.253)",
-
-                    textDark: "#ffffff",
-                    textMedium: "#b8b8b8",
-                    textLight: "#a0a0a0",
-                    textBubble: "#ffffff",
-
-                    bgIconHeader: "#b8b8b8",
-                    fgIconHeader: "#000000",
-                    textHeader: "#a1a1a1",
-                    textHeaderSelected: "#000000",
-
-                    bgCell: "#16161b",
-                    bgCellMedium: "#202027",
-                    bgHeader: "#212121",
-                    bgHeaderHasFocus: "#474747",
-                    bgHeaderHovered: "#404040",
-
-                    bgBubble: "#212121",
-                    bgBubbleSelected: "#000000",
-
-                    bgSearchResult: "#423c24",
-
-                    borderColor: "rgba(225,225,225,0.2)",
-                    drilldownBorder: "rgba(225,225,225,0.4)",
-
-                    linkColor: "#4F5DFF",
-
-                    headerFontStyle: "bold 14px",
-                    baseFontStyle: "13px",
-                    fontFamily:
-                        "Inter, Roboto, -apple-system, BlinkMacSystemFont, avenir next, avenir, segoe ui, helvetica neue, helvetica, Ubuntu, noto, arial, sans-serif",
-                }}
                 rows={500}
             />
         </BeautifulWrapper>
