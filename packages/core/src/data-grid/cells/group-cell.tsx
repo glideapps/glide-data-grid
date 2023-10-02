7119214307c89e1ba97c7904ed483faced8bfe76
Hovhannes Sargsyan <hsargsyan@Hovhanness-MacBook-Pro-2.local>
2023-10-02 15:00:39 +0400

    add comment

--- packages/core/src/data-grid/cells/group-cell.tsx
+++ packages/core/src/data-grid/cells/group-cell.tsx
@@ -30,10 +30,17 @@ export const groupRenderer: InternalCellRenderer<GroupCell> = {
     if (e.onRowDetailsUpdated) {
       let posX = e.posX;
       const iconXPosition = e.cell.level * e.theme.nestedGroupIndent + 4;
+      /**
+       * When working with group headers that contain spans and render cells, we've encountered an issue related to the position information. Specifically, when clicking on span 0, it correctly provides us with the postX value. However, when clicking on span 1, it returns the postX value of its cell instead.
+       * For example, if we click on a group header located within span 1, it gives us a postX value of approximately 22, which corresponds to the position within that span 1. However, what we actually need to determine is the location of our group icon. To address this, we should check if it's not in the first span. If it isn't, we should add the width of the row marker to accurately determine its position.
+       */
       const column = e.location[0];
       if (column !== 0 && e.rowMarkerWidth !== undefined) {
         posX = posX + e.rowMarkerWidth;
       }
+      /**
+       * The handling of the Glide doubleClick is currently managed through the mouseDown event, which, unfortunately, fires three times. Regrettably, we cannot prevent the event from firing. As a result, when a double-click event is triggered on the group icon, it fires multiple times, although it ultimately functions as expected.
+       */
       if (
         posX >= iconXPosition - GROUP_ICON_CLICK_PADDING &&
         posX <= iconXPosition + GROUP_ICON_CLICK_PADDING
