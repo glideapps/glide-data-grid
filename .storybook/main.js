aa33c0b01c0b1f8f2747b65902bffbe330b9f40e
Jason Smith <jason@heyglide.com>
2024-02-02 18:06:39 -0500

    Move storybook to vite

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,46 +1,28 @@
 const { dirname, join } = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const ResolveTypeScriptPlugin = require("resolve-typescript-plugin");
+const linaria = require("@linaria/vite");
+const vite = require("vite");
+
+const mergeConfig = vite.mergeConfig;
 
 module.exports = {
     stories: ["../**/src/**/*.stories.tsx"],
     addons: ["@storybook/addon-storysource", getAbsolutePath("@storybook/addon-controls")],
+    core: {
+        builder: "@storybook/builder-vite",
+    },
 
     typescript: {
         reactDocgen: false,
     },
 
-    webpackFinal: async (config, { configType }) => {
-        // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-        // You can change the configuration based on that.
-        // 'PRODUCTION' is used when building the static version of storybook.
-        // Check docs here: https://storybook.js.org/docs/react/configure/webpack#extending-storybooks-webpack-config
-
-        config.plugins.push(new MiniCssExtractPlugin({ filename: "[name].css" }));
-
-        // Add Linaria loader after babel-loader
-        config.module.rules.splice(1, 0, {
-            test: /\.tsx?$/,
-            exclude: /node_modules/,
-            use: [
-                {
-                    loader: require.resolve("@linaria/webpack-loader"),
-                    options: {
-                        sourceMap: true,
-                    },
-                },
-            ],
+    async viteFinal(config) {
+        return mergeConfig(config, {
+            plugins: [linaria.default()],
         });
-
-        config.optimization.minimize = false;
-        config.devtool = "source-map";
-        config.resolve.plugins = [new ResolveTypeScriptPlugin()];
-
-        return config;
     },
 
     framework: {
-        name: getAbsolutePath("@storybook/react-webpack5"),
+        name: getAbsolutePath("@storybook/react-vite"),
         options: {},
     },
 
