10b182718ee8d034498a925417821ca0830071c4
Jason Smith <jason@heyglide.com>
2024-02-03 19:00:11 -0800

    Make running storybook less cumbersome when working on cells/source packages

--- packages/core/build.sh
+++ packages/core/build.sh
@@ -9,18 +9,22 @@ shopt -s globstar
 ## Delete the dist folder
 rm -rf dist
 
+echo -e "\033[0;36mBuilding Glide Data Grid\033[0m"
+
 compile_esm() {
     tsc -p tsconfig.esm.json
-    linaria -r dist/esm/ -m esnext -o dist/esm/ dist/esm/**/*.js -t -i dist/esm -c ../../config/linaria.json    
+    linaria -r dist/esm/ -m esnext -o dist/esm/ dist/esm/**/*.js -t -i dist/esm -c ../../config/linaria.json > /dev/null
     remove_all_css_imports dist/esm
 }
 
 compile_cjs() {
     tsc -p tsconfig.cjs.json
-    linaria -r dist/cjs/ -m commonjs -o dist/cjs/ dist/cjs/**/*.js -t -i dist/cjs -c ../../config/linaria.json
+    linaria -r dist/cjs/ -m commonjs -o dist/cjs/ dist/cjs/**/*.js -t -i dist/cjs -c ../../config/linaria.json > /dev/null
     remove_all_css_imports dist/cjs
 }
 
 run_in_parallel compile_esm compile_cjs
 
 generate_index_css
+
+echo -e "\033[0;36mBuild Complete\033[0m"
