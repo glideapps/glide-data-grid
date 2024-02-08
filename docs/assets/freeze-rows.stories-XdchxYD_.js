1bac944bda4e2f94034f80490bdfdcd9dacdccaf
jassmith <jassmith@gmail.com>
2024-02-08 05:47:10 +0000

    Deploying to gh-pages from  @ 833753800e76fc8d677f43092a587e314aab6646 🚀

---
+++ docs/assets/freeze-rows.stories-XdchxYD_.js
@@ -0,0 +1,30 @@
+import{j as t,F as h}from"./marked.esm-dbrxtycE.js";import{R as c}from"./index-BMVQvedj.js";import{D as C}from"./data-editor-all-2MtuC67g.js";import{B as g,D as f,u as k,c as D,d as S}from"./utils-J-I08TtD.js";import{S as z}from"./story-utils-K2EZnGjM.js";import"./iframe-9FFu--7X.js";import"../sb-preview/runtime.js";import"./image-window-loader-lKUISrIQ.js";import"./throttle-7EuXLZa7.js";import"./_baseIteratee-WTHxv43n.js";import"./flatten-qRvRBp6y.js";import"./scrolling-data-grid-19OhoR-o.js";import"./index-PWBWJyi_.js";import"./index.esm-Ejw8GwRl.js";import"./index-wocATsGp.js";const _={title:"Glide-Data-Grid/DataEditor Demos",decorators:[o=>t(z,{children:t(g,{title:"Freeze rows",description:t(h,{children:t(f,{children:"Rows can be frozen to make sure the user always sees them."})}),children:t(o,{})})})]},r=()=>{const{cols:o,getCellContent:n,setCellValueRaw:l,setCellValue:m}=k(60,!1),[s,p]=c.useState(50),d=c.useCallback(()=>{const a=s;for(let e=0;e<o.length;e++){const R=n([e,a]);l([e,a],D(R))}p(e=>e+1)},[o.length,n,s,l]);return t(C,{...S,getCellContent:n,columns:o,rowMarkers:"both",freezeTrailingRows:2,experimental:{kineticScrollPerfHack:!0},onPaste:!0,onCellEdited:m,trailingRowOptions:{sticky:!0,tint:!0,hint:"New row..."},rows:s,onRowAppended:d})};var i,u,w;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`() => {
+  const {
+    cols,
+    getCellContent,
+    setCellValueRaw,
+    setCellValue
+  } = useMockDataGenerator(60, false);
+  const [numRows, setNumRows] = React.useState(50);
+  const onRowAppended = React.useCallback(() => {
+    const newRow = numRows;
+    // our data source is a mock source that pre-fills data, so we are just clearing this here. You should not
+    // need to do this.
+    for (let c = 0; c < cols.length; c++) {
+      const cell = getCellContent([c, newRow]);
+      setCellValueRaw([c, newRow], clearCell(cell));
+    }
+    // Tell the data grid there is another row
+    setNumRows(cv => cv + 1);
+  }, [cols.length, getCellContent, numRows, setCellValueRaw]);
+  return <DataEditor {...defaultProps} getCellContent={getCellContent} columns={cols} rowMarkers={"both"} freezeTrailingRows={2} experimental={{
+    kineticScrollPerfHack: true
+  }} onPaste={true} // we want to allow paste to just call onCellEdited
+  onCellEdited={setCellValue} // Sets the mock cell content
+  trailingRowOptions={{
+    // How to get the trailing row to look right
+    sticky: true,
+    tint: true,
+    hint: "New row..."
+  }} rows={numRows} onRowAppended={onRowAppended} />;
+}`,...(w=(u=r.parameters)==null?void 0:u.docs)==null?void 0:w.source}}};const v=["FreezeRows"];export{r as FreezeRows,v as __namedExportsOrder,_ as default};
---
+++ docs/assets/highlight-cells.stories-Dz0ASx6n.js
@@ -0,0 +1,33 @@
+import{j as e,a as p}from"./marked.esm-dbrxtycE.js";import{R as l}from"./index-BMVQvedj.js";import{D as u}from"./data-editor-all-2MtuC67g.js";import{B as S,D as C,P as f,u as w,d as D}from"./utils-J-I08TtD.js";import{C as s}from"./image-window-loader-lKUISrIQ.js";import{S as y}from"./story-utils-K2EZnGjM.js";import"./iframe-9FFu--7X.js";import"../sb-preview/runtime.js";import"./throttle-7EuXLZa7.js";import"./_baseIteratee-WTHxv43n.js";import"./flatten-qRvRBp6y.js";import"./scrolling-data-grid-19OhoR-o.js";import"./index-PWBWJyi_.js";import"./index.esm-Ejw8GwRl.js";import"./index-wocATsGp.js";const W={title:"Glide-Data-Grid/DataEditor Demos",decorators:[n=>e(y,{children:e(S,{title:"HighlightCells",description:p(C,{children:["The ",e(f,{children:"highlightRegions"})," prop can be set to provide additional hinting or context for the current selection."]}),children:e(n,{})})})]},r=()=>{const{cols:n,getCellContent:d}=w(100),[t,g]=l.useState({columns:s.empty(),rows:s.empty()}),m=l.useMemo(()=>{if(t.current===void 0)return;const[o,i]=t.current.cell;return[{color:"#44BB0022",range:{x:o+2,y:i,width:10,height:10},style:"solid"},{color:"#b000b021",range:{x:o,y:i+2,width:1,height:1}}]},[t]);return e(u,{...D,rowMarkers:"both",freezeColumns:1,highlightRegions:m,gridSelection:t,onGridSelectionChange:g,getCellContent:d,columns:n,verticalBorder:o=>o>0,rows:1e3})};var c,a,h;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`() => {
+  const {
+    cols,
+    getCellContent
+  } = useMockDataGenerator(100);
+  const [gridSelection, setGridSelection] = React.useState<GridSelection>({
+    columns: CompactSelection.empty(),
+    rows: CompactSelection.empty()
+  });
+  const highlights = React.useMemo<DataEditorProps["highlightRegions"]>(() => {
+    if (gridSelection.current === undefined) return undefined;
+    const [col, row] = gridSelection.current.cell;
+    return [{
+      color: "#44BB0022",
+      range: {
+        x: col + 2,
+        y: row,
+        width: 10,
+        height: 10
+      },
+      style: "solid"
+    }, {
+      color: "#b000b021",
+      range: {
+        x: col,
+        y: row + 2,
+        width: 1,
+        height: 1
+      }
+    }];
+  }, [gridSelection]);
+  return <DataEditor {...defaultProps} rowMarkers="both" freezeColumns={1} highlightRegions={highlights} gridSelection={gridSelection} onGridSelectionChange={setGridSelection} getCellContent={getCellContent} columns={cols} verticalBorder={c => c > 0} rows={1000} />;
+}`,...(h=(a=r.parameters)==null?void 0:a.docs)==null?void 0:h.source}}};const A=["HighlightCells"];export{r as HighlightCells,A as __namedExportsOrder,W as default};
