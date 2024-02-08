585e6ececdd055e6d0786e1e5fdce8396949721a
jassmith <jassmith@gmail.com>
2024-02-08 05:18:52 +0000

    Deploying to gh-pages from  @ eefdfd3826b7d06a9f305d16857ebc12cd81541c 🚀

---
+++ docs/assets/09-menus.stories-lZ_8bsRI.js
@@ -0,0 +1,273 @@
+import{j as n,a as s}from"./marked.esm-dbrxtycE.js";import{r as a}from"./index-BMVQvedj.js";import{G as S}from"./image-window-loader-2Fr7t2ou.js";import{D as h}from"./data-editor-all-v3SUrwQv.js";import{S as f}from"./story-utils-K2EZnGjM.js";import{D as O,M as p,H as g,W as b}from"./doc-wrapper-m1-LpzDL.js";import{u as E}from"./react-laag.esm-PpDllAFI.js";import"./throttle-7EuXLZa7.js";import"./_baseIteratee-WTHxv43n.js";import"./iframe-UFWDJmcQ.js";import"../sb-preview/runtime.js";import"./flatten-qRvRBp6y.js";import"./scrolling-data-grid-By3ovTFU.js";import"./toConsumableArray-ppDpjNRJ.js";import"./index-wocATsGp.js";const _={title:"Glide-Data-Grid/Docs",decorators:[r=>n(f,{children:n(r,{})})]},l=[{name:"Deidre Morris",company:"GONKLE",email:"deidremorris@gonkle.com",phone:"+1 (867) 507-3332"},{name:"Sheryl Craig",company:"EVENTAGE",email:"sherylcraig@eventage.com",phone:"+1 (869) 520-2227"},{name:"Lidia Bowers",company:"ANOCHA",email:"lidiabowers@anocha.com",phone:"+1 (808) 414-3826"},{name:"Jones Norton",company:"REPETWIRE",email:"jonesnorton@repetwire.com",phone:"+1 (875) 582-3320"},{name:"Lula Bruce",company:"COMDOM",email:"lulabruce@comdom.com",phone:"+1 (873) 452-2472"},{name:"Larsen Montgomery",company:"SQUISH",email:"larsenmontgomery@squish.com",phone:"+1 (893) 482-3651"},{name:"Becky Bright",company:"COMCUR",email:"beckybright@comcur.com",phone:"+1 (879) 494-2331"},{name:"Charlotte Rowland",company:"FROLIX",email:"charlotterowland@frolix.com",phone:"+1 (861) 439-2134"},{name:"Sonya Hensley",company:"GEEKETRON",email:"sonyahensley@geeketron.com",phone:"+1 (802) 553-2194"},{name:"Stephenson Guthrie",company:"EXOSWITCH",email:"stephensonguthrie@exoswitch.com",phone:"+1 (903) 449-3271"},{name:"Mcmillan Cline",company:"TURNLING",email:"mcmillancline@turnling.com",phone:"+1 (982) 496-2454"},{name:"Kemp Davis",company:"TETRATREX",email:"kempdavis@tetratrex.com",phone:"+1 (859) 594-2982"},{name:"Matilda Levy",company:"SLOFAST",email:"matildalevy@slofast.com",phone:"+1 (841) 521-2444"},{name:"Hattie Simpson",company:"COMTRAK",email:"hattiesimpson@comtrak.com",phone:"+1 (962) 587-3805"},{name:"Kinney Munoz",company:"IDETICA",email:"kinneymunoz@idetica.com",phone:"+1 (921) 513-2012"},{name:"Lambert Raymond",company:"TURNABOUT",email:"lambertraymond@turnabout.com",phone:"+1 (919) 519-2442"},{name:"Bryant Dunlap",company:"BYTREX",email:"bryantdunlap@bytrex.com",phone:"+1 (872) 583-2883"}],i=()=>{const r=a.useCallback(t=>{const[o,H]=t,m=l[H][["name","company","email","phone"][o]];return{kind:S.Text,allowOverlay:!0,displayData:m,data:m}},[]),d=a.useMemo(()=>[{title:"Name",id:"name",hasMenu:!0},{title:"Company",id:"company",hasMenu:!0},{title:"Email",id:"email",hasMenu:!0},{title:"Phone",id:"phone",hasMenu:!0}],[]),C=a.useCallback((t,o)=>{window.alert("Header menu clicked "+t+JSON.stringify(o))},[]),[e,u]=a.useState(),k=a.useCallback((t,o)=>{u({col:t,bounds:o})},[]),{renderLayer:R,layerProps:c}=E({isOpen:e!==void 0,triggerOffset:4,onOutsideClick:()=>u(void 0),trigger:{getBounds:()=>({bottom:((e==null?void 0:e.bounds.y)??0)+((e==null?void 0:e.bounds.height)??0),height:(e==null?void 0:e.bounds.height)??0,left:(e==null?void 0:e.bounds.x)??0,right:((e==null?void 0:e.bounds.x)??0)+((e==null?void 0:e.bounds.width)??0),top:(e==null?void 0:e.bounds.y)??0,width:(e==null?void 0:e.bounds.width)??0})},placement:"bottom-start",auto:!0,possiblePlacements:["bottom-start","bottom-end"]});return s(O,{children:[n(p,{children:`
+# Menus
+
+Glide Data Grid doesn't come with built in menus. Instead it is evented and ready to work with whatever menus you want 
+to use. Let's learn how to add basic menus using [react-laag](https://www.react-laag.com/). Adding menu drop down indicators to headers is as simple
+as passing a bool and listening to click events using \`onHeaderMenuClick\`.`}),n(g,{children:`
+const columns = React.useMemo<GridColumn[]>(() => {
+    return [
+        {
+            title: "Name",
+            id: "name",
+            hasMenu: true,
+        },
+        {
+            title: "Company",
+            id: "company",
+            hasMenu: true,
+        },
+        {
+            title: "Email",
+            id: "email",
+            hasMenu: true,
+        },
+        {
+            title: "Phone",
+            id: "phone",
+            hasMenu: true,
+        },
+    ];
+}, []);
+
+const onHeaderMenuClick = React.useCallback((col: number, position: Rectangle) => {
+    window.alert("Header menu clicked " + col + JSON.stringify(position));
+}, []);
+
+return <DataEditor {...rest} onHeaderMenuClick={onHeaderMenuClick} />;
+`}),n(b,{height:200,children:n(h,{getCellContent:r,columns:d,rows:l.length,onHeaderMenuClick:C})}),n(p,{children:`
+The provided coordinates are in page space. This makes it trivial to use [react-laag](https://www.react-laag.com/) to create a basic menu. Some 
+styling would go a long way here.`}),n(g,{children:`
+const [showMenu, setShowMenu] = React.useState<{ bounds: Rectangle; col: number }>();
+
+const onHeaderMenuClick = React.useCallback((col: number, bounds: Rectangle) => {
+    setShowMenu({ col, bounds });
+}, []);
+
+const { renderLayer, layerProps } = useLayer({
+    isOpen: showMenu !== undefined,
+    triggerOffset: 4,
+    onOutsideClick: () => setShowMenu(undefined),
+    trigger: {
+        getBounds: () => ({
+            bottom: (showMenu?.bounds.y ?? 0) + (showMenu?.bounds.height ?? 0),
+            height: showMenu?.bounds.height ?? 0,
+            left: showMenu?.bounds.x ?? 0,
+            right: (showMenu?.bounds.x ?? 0) + (showMenu?.bounds.width ?? 0),
+            top: showMenu?.bounds.y ?? 0,
+            width: showMenu?.bounds.width ?? 0,
+        }),
+    },
+    placement: "bottom-start",
+    auto: true,
+    possiblePlacements: ["bottom-start", "bottom-end"],
+    });
+
+return <>
+    <DataEditor {...rest} onHeaderMenuClick={onHeaderMenuClick} />
+    {showMenu !== undefined &&
+        renderLayer(
+            <div
+                {...layerProps}
+                style={{
+                    ...layerProps.style,
+                    width: 300,
+                    padding: 4,
+                    borderRadius: 8,
+                    backgroundColor: "white",
+                    border: "1px solid black",
+                }}>
+                <ul>
+                    <li>Item 1</li>
+                    <li>Item 2</li>
+                    <li>Item 3</li>
+                </ul>
+            </div>
+        )}
+</>;
+`}),s(b,{height:200,children:[n(h,{getCellContent:r,columns:d,rows:l.length,onHeaderMenuClick:k}),e!==void 0&&R(n("div",{...c,style:{...c.style,width:300,padding:4,borderRadius:8,backgroundColor:"white",border:"1px solid black"},children:s("ul",{children:[n("li",{children:"Item 1"}),n("li",{children:"Item 2"}),n("li",{children:"Item 3"})]})}))]})]})};i.storyName="09. Menus";var w,y,M;i.parameters={...i.parameters,docs:{...(w=i.parameters)==null?void 0:w.docs,source:{originalSource:`() => {
+  const getContent = React.useCallback((cell: Item): GridCell => {
+    const [col, row] = cell;
+    const dataRow = data[row];
+    const indexes: (keyof DummyItem)[] = ["name", "company", "email", "phone"];
+    const d = dataRow[indexes[col]];
+    return {
+      kind: GridCellKind.Text,
+      allowOverlay: true,
+      displayData: d,
+      data: d
+    };
+  }, []);
+  const columns = React.useMemo<GridColumn[]>(() => {
+    return [{
+      title: "Name",
+      id: "name",
+      hasMenu: true
+    }, {
+      title: "Company",
+      id: "company",
+      hasMenu: true
+    }, {
+      title: "Email",
+      id: "email",
+      hasMenu: true
+    }, {
+      title: "Phone",
+      id: "phone",
+      hasMenu: true
+    }];
+  }, []);
+  const onHeaderMenuClickedStage1 = React.useCallback((col: number, position: Rectangle) => {
+    window.alert("Header menu clicked " + col + JSON.stringify(position));
+  }, []);
+  const [showMenu, setShowMenu] = React.useState<{
+    bounds: Rectangle;
+    col: number;
+  }>();
+  const onHeaderMenuClickedStage2 = React.useCallback((col: number, bounds: Rectangle) => {
+    setShowMenu({
+      col,
+      bounds
+    });
+  }, []);
+  const {
+    renderLayer,
+    layerProps
+  } = useLayer({
+    isOpen: showMenu !== undefined,
+    triggerOffset: 4,
+    onOutsideClick: () => setShowMenu(undefined),
+    trigger: {
+      getBounds: () => ({
+        bottom: (showMenu?.bounds.y ?? 0) + (showMenu?.bounds.height ?? 0),
+        height: showMenu?.bounds.height ?? 0,
+        left: showMenu?.bounds.x ?? 0,
+        right: (showMenu?.bounds.x ?? 0) + (showMenu?.bounds.width ?? 0),
+        top: showMenu?.bounds.y ?? 0,
+        width: showMenu?.bounds.width ?? 0
+      })
+    },
+    placement: "bottom-start",
+    auto: true,
+    possiblePlacements: ["bottom-start", "bottom-end"]
+  });
+  return <DocWrapper>
+            <Marked>
+                {\`
+# Menus
+
+Glide Data Grid doesn't come with built in menus. Instead it is evented and ready to work with whatever menus you want 
+to use. Let's learn how to add basic menus using [react-laag](https://www.react-laag.com/). Adding menu drop down indicators to headers is as simple
+as passing a bool and listening to click events using \\\`onHeaderMenuClick\\\`.\`}
+            </Marked>
+            <Highlight>
+                {\`
+const columns = React.useMemo<GridColumn[]>(() => {
+    return [
+        {
+            title: "Name",
+            id: "name",
+            hasMenu: true,
+        },
+        {
+            title: "Company",
+            id: "company",
+            hasMenu: true,
+        },
+        {
+            title: "Email",
+            id: "email",
+            hasMenu: true,
+        },
+        {
+            title: "Phone",
+            id: "phone",
+            hasMenu: true,
+        },
+    ];
+}, []);
+
+const onHeaderMenuClick = React.useCallback((col: number, position: Rectangle) => {
+    window.alert("Header menu clicked " + col + JSON.stringify(position));
+}, []);
+
+return <DataEditor {...rest} onHeaderMenuClick={onHeaderMenuClick} />;
+\`}
+            </Highlight>
+            <Wrapper height={200}>
+                <DataEditor getCellContent={getContent} columns={columns} rows={data.length} onHeaderMenuClick={onHeaderMenuClickedStage1} />
+            </Wrapper>
+            <Marked>
+                {\`
+The provided coordinates are in page space. This makes it trivial to use [react-laag](https://www.react-laag.com/) to create a basic menu. Some 
+styling would go a long way here.\`}
+            </Marked>
+            <Highlight>
+                {\`
+const [showMenu, setShowMenu] = React.useState<{ bounds: Rectangle; col: number }>();
+
+const onHeaderMenuClick = React.useCallback((col: number, bounds: Rectangle) => {
+    setShowMenu({ col, bounds });
+}, []);
+
+const { renderLayer, layerProps } = useLayer({
+    isOpen: showMenu !== undefined,
+    triggerOffset: 4,
+    onOutsideClick: () => setShowMenu(undefined),
+    trigger: {
+        getBounds: () => ({
+            bottom: (showMenu?.bounds.y ?? 0) + (showMenu?.bounds.height ?? 0),
+            height: showMenu?.bounds.height ?? 0,
+            left: showMenu?.bounds.x ?? 0,
+            right: (showMenu?.bounds.x ?? 0) + (showMenu?.bounds.width ?? 0),
+            top: showMenu?.bounds.y ?? 0,
+            width: showMenu?.bounds.width ?? 0,
+        }),
+    },
+    placement: "bottom-start",
+    auto: true,
+    possiblePlacements: ["bottom-start", "bottom-end"],
+    });
+
+return <>
+    <DataEditor {...rest} onHeaderMenuClick={onHeaderMenuClick} />
+    {showMenu !== undefined &&
+        renderLayer(
+            <div
+                {...layerProps}
+                style={{
+                    ...layerProps.style,
+                    width: 300,
+                    padding: 4,
+                    borderRadius: 8,
+                    backgroundColor: "white",
+                    border: "1px solid black",
+                }}>
+                <ul>
+                    <li>Item 1</li>
+                    <li>Item 2</li>
+                    <li>Item 3</li>
+                </ul>
+            </div>
+        )}
+</>;
+\`}
+            </Highlight>
+            <Wrapper height={200}>
+                <DataEditor getCellContent={getContent} columns={columns} rows={data.length} onHeaderMenuClick={onHeaderMenuClickedStage2} />
+                {showMenu !== undefined && renderLayer(<div {...layerProps} style={{
+        ...layerProps.style,
+        width: 300,
+        padding: 4,
+        borderRadius: 8,
+        backgroundColor: "white",
+        border: "1px solid black"
+      }}>
+                            <ul>
+                                <li>Item 1</li>
+                                <li>Item 2</li>
+                                <li>Item 3</li>
+                            </ul>
+                        </div>)}
+            </Wrapper>
+        </DocWrapper>;
+}`,...(M=(y=i.parameters)==null?void 0:y.docs)==null?void 0:M.source}}};const z=["Menus"];export{i as Menus,z as __namedExportsOrder,_ as default};
---
+++ docs/assets/server-side-data.stories-3MIDh6aQ.js
@@ -0,0 +1,35 @@
+import{j as x}from"./marked.esm-dbrxtycE.js";import{g as S,R as l}from"./index-BMVQvedj.js";import{G as y,C as $}from"./image-window-loader-2Fr7t2ou.js";import{S as B}from"./story-utils-K2EZnGjM.js";import{D as W}from"./data-editor-all-v3SUrwQv.js";import{a as F,b as O,r as C}from"./throttle-7EuXLZa7.js";import{B as V}from"./utils-_crEUr6w.js";import{a as j}from"./doc-wrapper-m1-LpzDL.js";import"./iframe-UFWDJmcQ.js";import"../sb-preview/runtime.js";import"./flatten-qRvRBp6y.js";import"./_baseIteratee-WTHxv43n.js";import"./scrolling-data-grid-By3ovTFU.js";import"./index-PWBWJyi_.js";import"./index.esm-Ejw8GwRl.js";import"./index-wocATsGp.js";import"./toConsumableArray-ppDpjNRJ.js";function K(e,t,a){var c=-1,r=e.length;t<0&&(t=-t>r?0:r+t),a=a>r?r:a,a<0&&(a+=r),r=t>a?0:a-t>>>0,t>>>=0;for(var o=Array(r);++c<r;)o[c]=e[c+t];return o}var L=K,q=F;function H(e){var t=q(e),a=t%1;return t===t?a?t-a:t:0}var J=H,N=L,Q=O,U=J,X=Math.ceil,Y=Math.max;function Z(e,t,a){(a?Q(e,t,a):t===void 0)?t=1:t=Y(U(t),0);var c=e==null?0:e.length;if(!c||t<1)return[];for(var r=0,o=0,m=Array(X(c/t));r<c;)m[o++]=N(e,r,r+=t);return m}var z=Z;const ee=S(z),ke={title:"Glide-Data-Grid/DataEditor Demos",decorators:[e=>x(B,{children:x(e,{})})]};function te(e,t,a,c,r,o){e=Math.max(e,1);const m=l.useRef($.empty()),h=l.useRef([]),[f,G]=l.useState({x:0,y:0,width:0,height:0}),b=l.useRef(f);b.current=f;const T=l.useCallback(n=>{G(s=>n.x===s.x&&n.y===s.y&&n.width===s.width&&n.height===s.height?s:n)},[]),v=l.useCallback(n=>{const[s,d]=n,i=h.current[d];return i!==void 0?c(i,s):{kind:y.Loading,allowOverlay:!1}},[c]),k=l.useCallback(async n=>{var p;m.current=m.current.add(n);const s=n*e,d=await a([s,(n+1)*e]),i=b.current,u=[],w=h.current;for(const[R,E]of d.entries()){w[R+s]=E;for(let D=i.x;D<=i.x+i.width;D++)u.push({cell:[D,R+s]})}(p=o.current)==null||p.updateCells(u)},[a,o,e]),_=l.useCallback(n=>async()=>{const s=Math.max(0,Math.floor(n.y/e)),d=Math.floor((n.y+n.height)/e);for(const u of ee(C(s,d+1).filter(w=>!m.current.hasIndex(w)),t))await Promise.allSettled(u.map(k));const i=[];for(let u=n.y;u<n.y+n.height;u++){const w=[];for(let p=n.x;p<n.x+n.width;p++)w.push(v([p,u]));i.push(w)}return i},[v,k,t,e]);l.useEffect(()=>{const n=f,s=Math.max(0,Math.floor((n.y-e/2)/e)),d=Math.floor((n.y+n.height+e/2)/e);for(const i of C(s,d+1))m.current.hasIndex(i)||k(i)},[k,e,f]);const A=l.useCallback((n,s)=>{const[,d]=n,i=h.current[d];if(i===void 0)return;const u=r(n,s,i);u!==void 0&&(h.current[d]=u)},[r]);return{getCellContent:v,onVisibleRegionChanged:T,onCellEdited:A,getCellsForSelection:_}}const g=()=>{const e=l.useRef(null),t=l.useCallback(async r=>(await new Promise(o=>setTimeout(o,300)),C(r[0],r[1]).map(o=>[`1, ${o}`,`2, ${o}`])),[]),a=l.useMemo(()=>[{title:"A",width:150},{title:"B",width:200}],[]),c=te(50,5,t,l.useCallback((r,o)=>({kind:y.Text,data:r[o],allowOverlay:!0,displayData:r[o]}),[]),l.useCallback((r,o,m)=>{const[h]=r;if(o.kind!==y.Text)return;const f=[...m];return f[h]=o.data,f},[]),e);return x(V,{title:"Server Side Data",description:x(j,{children:"Glide data grid is fully ready to handle your server side data needs. This example condenses the implementation into a single custom hook and loads in pages of 50. We are using 300ms sleeps, but network transactions should work the same."}),children:x(W,{ref:e,...c,width:"100%",columns:a,rows:3e3,rowMarkers:"both"})})};g.parameters={options:{showPanel:!1}};var M,I,P;g.parameters={...g.parameters,docs:{...(M=g.parameters)==null?void 0:M.docs,source:{originalSource:`() => {
+  const ref = React.useRef<DataEditorRef | null>(null);
+  const getRowData = React.useCallback(async (r: Item) => {
+    await new Promise(res => setTimeout(res, 300));
+    return range(r[0], r[1]).map(rowIndex => [\`1, \${rowIndex}\`, \`2, \${rowIndex}\`]);
+  }, []);
+  const columns = React.useMemo<readonly GridColumn[]>(() => {
+    return [{
+      title: "A",
+      width: 150
+    }, {
+      title: "B",
+      width: 200
+    }];
+  }, []);
+  const args = useAsyncData<string[]>(50, 5, getRowData, React.useCallback((rowData, col) => ({
+    kind: GridCellKind.Text,
+    data: rowData[col],
+    allowOverlay: true,
+    displayData: rowData[col]
+  }), []), React.useCallback((cell, newVal, rowData) => {
+    const [col] = cell;
+    if (newVal.kind !== GridCellKind.Text) return undefined;
+    const newRow: string[] = [...rowData];
+    newRow[col] = newVal.data;
+    return newRow;
+  }, []), ref);
+  return <BeautifulWrapper title="Server Side Data" description={<Description>
+                    Glide data grid is fully ready to handle your server side data needs. This example condenses the
+                    implementation into a single custom hook and loads in pages of 50. We are using 300ms sleeps, but
+                    network transactions should work the same.
+                </Description>}>
+            <DataEditor ref={ref} {...args} width="100%" columns={columns} rows={3000} rowMarkers="both" />
+        </BeautifulWrapper>;
+}`,...(P=(I=g.parameters)==null?void 0:I.docs)==null?void 0:P.source}}};const ve=["ServerSideData"];export{g as ServerSideData,ve as __namedExportsOrder,ke as default};
