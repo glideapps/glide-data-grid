6609abf21da14f7cb91a295dab570cd4e54d858d
jassmith <jassmith@gmail.com>
2024-02-10 05:30:40 +0000

    Deploying to gh-pages from  @ 752f8f87356702656b863fac8c44b32d0306b3af 🚀

---
+++ docs/assets/append-row-handle.stories-YQyg-27A.js
@@ -0,0 +1,36 @@
+import{j as e,a as c,F as k}from"./marked.esm-dbrxtycE.js";import{R as n}from"./index-BMVQvedj.js";import{D}from"./data-editor-all-MRDpHEQ_.js";import{u as N,c as y,B as A,D as b,P as p,M as P,K as E,d as M}from"./utils-0sbaJdQp.js";import{S as V}from"./story-utils-K2EZnGjM.js";import"./iframe-MICdw5w5.js";import"../sb-preview/runtime.js";import"./image-window-loader-aCHQvgUN.js";import"./throttle-7EuXLZa7.js";import"./_baseIteratee-WTHxv43n.js";import"./flatten-qRvRBp6y.js";import"./scrolling-data-grid-UJ5_dX2R.js";import"./index-PWBWJyi_.js";import"./index.esm-Ejw8GwRl.js";import"./index-wocATsGp.js";const z={title:"Glide-Data-Grid/DataEditor Demos",decorators:[a=>e(V,{children:e(a,{})})]},r=()=>{const{cols:a,getCellContent:l,setCellValueRaw:d,setCellValue:R}=N(60,!1),[s,f]=n.useState(50),i=n.useRef(null),C=n.useCallback(()=>{var t;(t=i.current)==null||t.appendRow(3,!1)},[i]),g=n.useCallback(()=>{const t=s;for(let o=0;o<6;o++){const h=l([o,t]);d([o,t],y(h))}f(o=>o+1)},[l,s,d]);return e(A,{title:"appendRow Ref",description:c(k,{children:[c(b,{children:["Adding data can also be triggered from outside of ",e(p,{children:"DataEditor"})]}),c(P,{children:["By calling ",e(p,{children:"appendRow"})," on a ",e(p,{children:"ref"})," to your grid, you can trigger the append elsewhere, like this ",e(E,{onClick:C,children:"Append"})," button"]})]}),children:e(D,{...M,ref:i,getCellContent:l,columns:a,rowMarkers:"both",onCellEdited:R,trailingRowOptions:{hint:"New row...",sticky:!0,tint:!0},rows:s,onRowAppended:g})})};var u,m,w;r.parameters={...r.parameters,docs:{...(u=r.parameters)==null?void 0:u.docs,source:{originalSource:`() => {
+  const {
+    cols,
+    getCellContent,
+    setCellValueRaw,
+    setCellValue
+  } = useMockDataGenerator(60, false);
+  const [numRows, setNumRows] = React.useState(50);
+  const ref = React.useRef<DataEditorRef>(null);
+  const onClick = React.useCallback(() => {
+    void ref.current?.appendRow(3, false);
+  }, [ref]);
+  const onRowAppended = React.useCallback(() => {
+    const newRow = numRows;
+    for (let c = 0; c < 6; c++) {
+      const cell = getCellContent([c, newRow]);
+      setCellValueRaw([c, newRow], clearCell(cell));
+    }
+    setNumRows(cv => cv + 1);
+  }, [getCellContent, numRows, setCellValueRaw]);
+  return <BeautifulWrapper title="appendRow Ref" description={<>
+                    <Description>
+                        Adding data can also be triggered from outside of <PropName>DataEditor</PropName>
+                    </Description>
+                    <MoreInfo>
+                        By calling <PropName>appendRow</PropName> on a <PropName>ref</PropName> to your grid, you can
+                        trigger the append elsewhere, like this <KeyName onClick={onClick}>Append</KeyName> button
+                    </MoreInfo>
+                </>}>
+            <DataEditor {...defaultProps} ref={ref} getCellContent={getCellContent} columns={cols} rowMarkers={"both"} onCellEdited={setCellValue} trailingRowOptions={{
+      hint: "New row...",
+      sticky: true,
+      tint: true
+    }} rows={numRows} onRowAppended={onRowAppended} />
+        </BeautifulWrapper>;
+}`,...(w=(m=r.parameters)==null?void 0:m.docs)==null?void 0:w.source}}};const J=["AppendRowHandle"];export{r as AppendRowHandle,J as __namedExportsOrder,z as default};
--- docs/assets/freeze-columns.stories-VUOxlg1v.js
+++
@@ -1,9 +0,0 @@
-import{j as r,a as m}from"./marked.esm-dbrxtycE.js";import"./index-BMVQvedj.js";import{D as i}from"./data-editor-all-2MtuC67g.js";import{B as p,D as c,P as u,u as d,d as f}from"./utils-J-I08TtD.js";import{S as C}from"./story-utils-K2EZnGjM.js";import"./iframe-9FFu--7X.js";import"../sb-preview/runtime.js";import"./image-window-loader-lKUISrIQ.js";import"./throttle-7EuXLZa7.js";import"./_baseIteratee-WTHxv43n.js";import"./flatten-qRvRBp6y.js";import"./scrolling-data-grid-19OhoR-o.js";import"./index-PWBWJyi_.js";import"./index.esm-Ejw8GwRl.js";import"./index-wocATsGp.js";const F={title:"Glide-Data-Grid/DataEditor Demos",decorators:[o=>r(C,{children:r(p,{title:"Freeze columns",description:m(c,{children:["Columns at the start of your grid can be frozen in place by settings"," ",r(u,{children:"freezeColumns"})," to a number greater than 0."]}),children:r(o,{})})})]},e=o=>{const{cols:n,getCellContent:l}=d(100);return r(i,{...f,rowMarkers:"both",freezeColumns:o.freezeColumns,getCellContent:l,columns:n,verticalBorder:!1,rows:1e3})};e.argTypes={freezeColumns:{control:{type:"range",min:0,max:10}}};e.args={freezeColumns:1};var t,s,a;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`(p: {
-  freezeColumns: number;
-}) => {
-  const {
-    cols,
-    getCellContent
-  } = useMockDataGenerator(100);
-  return <DataEditor {...defaultProps} rowMarkers="both" freezeColumns={p.freezeColumns} getCellContent={getCellContent} columns={cols} verticalBorder={false} rows={1000} />;
-}`,...(a=(s=e.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};const S=["FreezeColumns"];export{e as FreezeColumns,S as __namedExportsOrder,F as default};
