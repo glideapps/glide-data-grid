394a6ac76d328a912c502e3f580d24050ea9173f
jassmith <jassmith@gmail.com>
2024-02-14 03:58:10 +0000

    Deploying to gh-pages from  @ cf6c69a7f79427adb69e88bb099ae1e43b87e706 🚀

---
+++ docs/assets/all-cell-kinds.stories-ljbwmHHG.js
@@ -0,0 +1,19 @@
+import{j as e,a as d}from"./marked.esm-dbrxtycE.js";import"./index-BMVQvedj.js";import{D as m}from"./data-editor-all-3W5wpw6K.js";import{B as p,D as c,P as u,a as C,d as h}from"./utils--Nf55hQd.js";import{S as g}from"./story-utils-K2EZnGjM.js";import"./iframe-MU0yo5By.js";import"../sb-preview/runtime.js";import"./image-window-loader-Hk1rG8Sr.js";import"./throttle-7EuXLZa7.js";import"./_baseIteratee-WTHxv43n.js";import"./flatten-qRvRBp6y.js";import"./scrolling-data-grid-6oMlsc5s.js";import"./index-PWBWJyi_.js";import"./index.esm-Ejw8GwRl.js";import"./index-wocATsGp.js";const V={title:"Glide-Data-Grid/DataEditor Demos",decorators:[o=>e(g,{children:e(p,{title:"Lotsa cell kinds",description:d(c,{children:["Data grid supports plenty cell kinds. Anything under ",e(u,{children:"GridCellKind"}),"."]}),children:e(o,{})})})]},t=()=>{const{cols:o,getCellContent:s,onColumnResize:i,setCellValue:a}=C();return e(m,{...h,getCellContent:s,columns:o,onCellEdited:a,onPaste:!0,onColumnResize:i,highlightRegions:[{color:"#ff00ff33",range:{x:1,y:1,width:3,height:3}}],rows:1e3})};var l,r,n;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`() => {
+  const {
+    cols,
+    getCellContent,
+    onColumnResize,
+    setCellValue
+  } = useAllMockedKinds();
+  return <DataEditor {...defaultProps} getCellContent={getCellContent} columns={cols} onCellEdited={setCellValue} onPaste={true}
+  // rowHeight={55}
+  onColumnResize={onColumnResize} highlightRegions={[{
+    color: "#ff00ff33",
+    range: {
+      x: 1,
+      y: 1,
+      width: 3,
+      height: 3
+    }
+  }]} rows={1000} />;
+}`,...(n=(r=t.parameters)==null?void 0:r.docs)==null?void 0:n.source}}};const _=["AllCellKinds"];export{t as AllCellKinds,_ as __namedExportsOrder,V as default};
---
+++ docs/assets/padding.stories-gGAkrL6U.js
@@ -0,0 +1,14 @@
+import{j as t,F as g,a as l}from"./marked.esm-dbrxtycE.js";import"./index-BMVQvedj.js";import{D as c}from"./data-editor-all-3W5wpw6K.js";import{B as h,D as u,P as e,u as D,d as f}from"./utils--Nf55hQd.js";import{S as x}from"./story-utils-K2EZnGjM.js";import"./iframe-MU0yo5By.js";import"../sb-preview/runtime.js";import"./image-window-loader-Hk1rG8Sr.js";import"./throttle-7EuXLZa7.js";import"./_baseIteratee-WTHxv43n.js";import"./flatten-qRvRBp6y.js";import"./scrolling-data-grid-6oMlsc5s.js";import"./index-PWBWJyi_.js";import"./index.esm-Ejw8GwRl.js";import"./index-wocATsGp.js";const T={title:"Glide-Data-Grid/DataEditor Demos",decorators:[r=>t(x,{children:t(h,{title:"Padding",description:t(g,{children:l(u,{children:["You can add padding at the ends of the grid by setting the"," ",t(e,{children:"paddingRight"})," and ",t(e,{children:"paddingBottom"})," props"]})}),children:t(r,{})})})]},o=r=>{const{paddingRight:d,paddingBottom:s}=r,{cols:p,getCellContent:m}=D(20);return t(c,{...f,getCellContent:m,columns:p,rowMarkers:"both",experimental:{paddingRight:d,paddingBottom:s},rows:50})};o.argTypes={paddingRight:{control:{type:"range",min:0,max:600}},paddingBottom:{control:{type:"range",min:0,max:600}}};o.args={paddingRight:200,paddingBottom:200};var a,n,i;o.parameters={...o.parameters,docs:{...(a=o.parameters)==null?void 0:a.docs,source:{originalSource:`p => {
+  const {
+    paddingRight,
+    paddingBottom
+  } = p;
+  const {
+    cols,
+    getCellContent
+  } = useMockDataGenerator(20);
+  return <DataEditor {...defaultProps} getCellContent={getCellContent} columns={cols} rowMarkers={"both"} experimental={{
+    paddingRight,
+    paddingBottom
+  }} rows={50} />;
+}`,...(i=(n=o.parameters)==null?void 0:n.docs)==null?void 0:i.source}}};const W=["Padding"];export{o as Padding,W as __namedExportsOrder,T as default};
