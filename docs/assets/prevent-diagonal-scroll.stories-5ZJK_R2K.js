92e8b94f4918aea1766b371f5b345e3a7fef0a26
jassmith <jassmith@gmail.com>
2024-03-07 19:59:33 +0000

    Deploying to gh-pages from  @ 73e63f595be50d70d67becc65fdb3d5203b52c08 🚀

--- docs/assets/prevent-diagonal-scroll.stories-5ZJK_R2K.js
+++
@@ -1,7 +0,0 @@
-import{j as r,F as i,a as s}from"./marked.esm-dbrxtycE.js";import"./index-BMVQvedj.js";import{D as p}from"./data-editor-all-7B89JiwZ.js";import{B as c,D as m,P as d,u as g,d as D}from"./utils-4-j8O3Q9.js";import{S as u}from"./story-utils-K2EZnGjM.js";import"./iframe-C8A8SeMs.js";import"../sb-preview/runtime.js";import"./image-window-loader-AqdSaR7d.js";import"./throttle-7EuXLZa7.js";import"./_baseIteratee-WTHxv43n.js";import"./flatten-qRvRBp6y.js";import"./scrolling-data-grid-ex7RycJy.js";import"./index-PWBWJyi_.js";import"./index.esm-Ejw8GwRl.js";import"./index-wocATsGp.js";const F={title:"Glide-Data-Grid/DataEditor Demos",decorators:[o=>r(u,{children:r(c,{title:"Prevent Diagonal Scroll",description:r(i,{children:s(m,{children:["Diagonal scrolling can be prevented by setting"," ",r(d,{children:"preventDiagonalScrolling"}),"."]})}),children:r(o,{})})})]},e=()=>{const{cols:o,getCellContent:l}=g(200);return r(p,{...D,getCellContent:l,columns:o,preventDiagonalScrolling:!0,rows:5e3})};var t,a,n;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`() => {
-  const {
-    cols,
-    getCellContent
-  } = useMockDataGenerator(200);
-  return <DataEditor {...defaultProps} getCellContent={getCellContent} columns={cols} preventDiagonalScrolling={true} rows={5000} />;
-}`,...(n=(a=e.parameters)==null?void 0:a.docs)==null?void 0:n.source}}};const M=["PreventDiagonalScroll"];export{e as PreventDiagonalScroll,M as __namedExportsOrder,F as default};
---
+++ docs/assets/reorder-rows.stories-Fk12RqcX.js
@@ -0,0 +1,29 @@
+import{j as o,F as C,a as D}from"./marked.esm-dbrxtycE.js";import{l as f}from"./lodash-8jIGfDyZ.js";import{R as t}from"./index-BMVQvedj.js";import{D as h}from"./data-editor-all-7qGf1nmg.js";import{B as b,D as g,P as v,d as k}from"./utils-IRSZKvf9.js";import{G as x}from"./image-window-loader-nvLgsaxj.js";import{S as M}from"./story-utils-K2EZnGjM.js";import"./iframe-AiCMyApU.js";import"../sb-preview/runtime.js";import"./throttle-7EuXLZa7.js";import"./_baseIteratee-WTHxv43n.js";import"./flatten-qRvRBp6y.js";import"./scrolling-data-grid-SWWsfOyf.js";import"./index-PWBWJyi_.js";import"./index.esm-Ejw8GwRl.js";import"./index-wocATsGp.js";const q={title:"Glide-Data-Grid/DataEditor Demos",decorators:[s=>o(M,{children:o(b,{title:"Reorder Rows",description:o(C,{children:D(g,{children:["Rows can be re-arranged by using the ",o(v,{children:"onRowMoved"})," callback. When set the first row can be used to drag and drop."]})}),children:o(s,{})})})]},a=()=>{const s=t.useMemo(()=>[{title:"Col A",width:150},{title:"Col B",width:150}],[]),[n,m]=t.useState(()=>f.range(0,50).map(e=>[`A: ${e}`,`B: ${e}`])),p=t.useCallback(([e,r])=>({kind:x.Text,allowOverlay:!1,data:n[r][e],displayData:n[r][e]}),[n]),w=t.useCallback((e,r)=>{m(u=>{const l=[...u],R=l.splice(e,1);return l.splice(r,0,...R),l})},[]);return o(h,{...k,rowMarkers:"both",onRowMoved:w,getCellContent:p,columns:s,rows:50})};var d,i,c;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`() => {
+  const cols = React.useMemo<GridColumn[]>(() => [{
+    title: "Col A",
+    width: 150
+  }, {
+    title: "Col B",
+    width: 150
+  }], []);
+  const [rowData, setRowData] = React.useState(() => {
+    return range(0, 50).map(x => [\`A: \${x}\`, \`B: \${x}\`]);
+  });
+  const getCellContent = React.useCallback<DataEditorProps["getCellContent"]>(([col, row]) => {
+    return {
+      kind: GridCellKind.Text,
+      allowOverlay: false,
+      data: rowData[row][col],
+      displayData: rowData[row][col]
+    };
+  }, [rowData]);
+  const reorderRows = React.useCallback((from: number, to: number) => {
+    setRowData(cv => {
+      const d = [...cv];
+      const removed = d.splice(from, 1);
+      d.splice(to, 0, ...removed);
+      return d;
+    });
+  }, []);
+  return <DataEditor {...defaultProps} rowMarkers={"both"} onRowMoved={reorderRows} getCellContent={getCellContent} columns={cols} rows={50} />;
+}`,...(c=(i=a.parameters)==null?void 0:i.docs)==null?void 0:c.source}}};const z=["ReorderRows"];export{a as ReorderRows,z as __namedExportsOrder,q as default};
