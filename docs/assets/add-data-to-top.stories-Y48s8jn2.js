16a3e4a463c8c7ab76068fd0bc61d32cd5a2eb40
jassmith <jassmith@gmail.com>
2024-03-13 18:34:06 +0000

    Deploying to gh-pages from  @ a57eda16cee8abd08b697b3bda5c5e347b8c40b6 🚀

---
+++ docs/assets/add-data-to-top.stories-Y48s8jn2.js
@@ -0,0 +1,28 @@
+import{j as o,F as C}from"./marked.esm-dbrxtycE.js";import{R as c}from"./index-BMVQvedj.js";import{D as R}from"./data-editor-all-ZeeWpldS.js";import{B as f,D as h,u as D,c as g,d as x}from"./utils-MADpeXg5.js";import{S as y}from"./story-utils-K2EZnGjM.js";import"./iframe-alOpk4ZG.js";import"../sb-preview/runtime.js";import"./image-window-loader-ggeJlWLo.js";import"./throttle-7EuXLZa7.js";import"./_baseIteratee-WTHxv43n.js";import"./flatten-qRvRBp6y.js";import"./scrolling-data-grid-wnX0bxop.js";import"./index-PWBWJyi_.js";import"./index.esm-Ejw8GwRl.js";import"./index-wocATsGp.js";const F={title:"Glide-Data-Grid/DataEditor Demos",decorators:[l=>o(y,{children:o(f,{title:"Add data",description:o(C,{children:o(h,{children:"You can return a different location to have the new row append take place."})}),children:o(l,{})})})]},r=()=>{const{cols:l,getCellContent:n,setCellValueRaw:a,setCellValue:u}=D(60,!1),[s,m]=c.useState(50),w=c.useCallback(async()=>{for(let e=s;e>0;e--)for(let t=0;t<6;t++)a([t,e],n([t,e-1]));for(let e=0;e<6;e++){const t=n([e,0]);a([e,0],g(t))}return m(e=>e+1),"top"},[n,s,a]);return o(R,{...x,getCellContent:n,columns:l,rowMarkers:"both",onCellEdited:u,trailingRowOptions:{hint:"New row...",sticky:!0,tint:!0},rows:s,onRowAppended:w})};var i,p,d;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`() => {
+  const {
+    cols,
+    getCellContent,
+    setCellValueRaw,
+    setCellValue
+  } = useMockDataGenerator(60, false);
+  const [numRows, setNumRows] = React.useState(50);
+  const onRowAppended = React.useCallback(async () => {
+    // shift all of the existing cells down
+    for (let y = numRows; y > 0; y--) {
+      for (let x = 0; x < 6; x++) {
+        setCellValueRaw([x, y], getCellContent([x, y - 1]));
+      }
+    }
+    for (let c = 0; c < 6; c++) {
+      const cell = getCellContent([c, 0]);
+      setCellValueRaw([c, 0], clearCell(cell));
+    }
+    setNumRows(cv => cv + 1);
+    return ("top" as const);
+  }, [getCellContent, numRows, setCellValueRaw]);
+  return <DataEditor {...defaultProps} getCellContent={getCellContent} columns={cols} rowMarkers={"both"} onCellEdited={setCellValue} trailingRowOptions={{
+    hint: "New row...",
+    sticky: true,
+    tint: true
+  }} rows={numRows} onRowAppended={onRowAppended} />;
+}`,...(d=(p=r.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};const P=["AddDataToTop"];export{r as AddDataToTop,P as __namedExportsOrder,F as default};
---
+++ docs/assets/data-editor-repros.stories-9nGWjuU7.js
@@ -0,0 +1,31 @@
+import{j as t,a as m,s as w}from"./marked.esm-dbrxtycE.js";import"./index-BMVQvedj.js";import{B as f}from"./story-utils-K2EZnGjM.js";import{G as p}from"./image-window-loader-ggeJlWLo.js";import{D as g}from"./data-editor-all-ZeeWpldS.js";import"./throttle-7EuXLZa7.js";import"./_baseIteratee-WTHxv43n.js";import"./iframe-alOpk4ZG.js";import"../sb-preview/runtime.js";import"./flatten-qRvRBp6y.js";import"./scrolling-data-grid-wnX0bxop.js";const{useState:T,useMemo:S}=__STORYBOOK_MODULE_ADDONS__,$={title:"Tests/TestCases/Bugs",decorators:[e=>t(f,{width:1e3,height:800,children:t(e,{})})]},A=([,e])=>({allowOverlay:!0,kind:p.Number,data:e,displayData:e.toString()}),x=()=>{},v=w("div")({name:"Bug70Style",class:"b1nvh7n2",propsAsIs:!1});function o(){return m(v,{className:"App",children:[t("p",{children:"To cause error: scroll down at least one row, edit a cell in Col2, and hit Tab"}),t("a",{href:"https://github.com/glideapps/glide-data-grid/issues/70",target:"_blank",rel:"noreferrer",children:"Original report"}),t(g,{width:500,height:500,rows:100,columns:[{title:"Col1",width:100},{title:"Col2",width:100}],getCellContent:A,onCellEdited:x})]})}const _=([e,r])=>({allowOverlay:!0,kind:p.Text,data:`${e} - ${r}`,displayData:`${e} - ${r}`}),l=[{title:"Col AAAA",width:120},{title:"Col AAA",width:120},{title:"Col AA",width:120},{title:"Col A",width:120},{title:"Col",width:120}];function n(){const[e,r]=T(""),C=S(()=>e===""?l:l.filter(s=>s.title.toLowerCase().includes(e.toLowerCase())),[e]);return m("div",{children:[t("input",{value:e,onChange:s=>{r(s.target.value)}}),t(g,{width:1e3,height:500,rows:100,columns:C,getCellContent:_,smoothScrollX:!0,smoothScrollY:!0})]})}var a,i,c;o.parameters={...o.parameters,docs:{...(a=o.parameters)==null?void 0:a.docs,source:{originalSource:`function Bug70() {
+  const cols = [{
+    title: "Col1",
+    width: 100
+  }, {
+    title: "Col2",
+    width: 100
+  }];
+  return <Bug70Style className="App">
+            <p>To cause error: scroll down at least one row, edit a cell in Col2, and hit Tab</p>
+            <a href="https://github.com/glideapps/glide-data-grid/issues/70" target="_blank" rel="noreferrer">
+                Original report
+            </a>
+            <DataEditor width={500} height={500} rows={100} columns={cols} getCellContent={bug70Gen} onCellEdited={ignore} />
+        </Bug70Style>;
+}`,...(c=(i=o.parameters)==null?void 0:i.docs)==null?void 0:c.source}}};var u,d,h;n.parameters={...n.parameters,docs:{...(u=n.parameters)==null?void 0:u.docs,source:{originalSource:`function FilterColumns() {
+  const [searchText, setSearchText] = useState("");
+  const cols = useMemo(() => {
+    if (searchText === "") {
+      return filteringColumns;
+    }
+    return filteringColumns.filter(c => c.title.toLowerCase().includes(searchText.toLowerCase()));
+  }, [searchText]);
+  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value);
+  };
+  return <div>
+            <input value={searchText} onChange={onInputChange} />
+            <DataEditor width={1000} height={500} rows={100} columns={cols} getCellContent={filterColumnsGen} smoothScrollX={true} smoothScrollY={true} />
+        </div>;
+}`,...(h=(d=n.parameters)==null?void 0:d.docs)==null?void 0:h.source}}};const j=["Bug70","FilterColumns"];export{o as Bug70,n as FilterColumns,j as __namedExportsOrder,$ as default};
