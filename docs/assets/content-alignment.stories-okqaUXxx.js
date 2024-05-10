cdb1efdc2dac22183816f78601cf0f15b8b345ae
jassmith <jassmith@gmail.com>
2024-05-10 20:29:01 +0000

    Deploying to gh-pages from  @ c21e6ca1988b56caac4eb2dca1f0c5abe0fd17ef 🚀

---
+++ docs/assets/content-alignment.stories-okqaUXxx.js
@@ -0,0 +1,29 @@
+import{j as t,a as p}from"./marked.esm-dbrxtycE.js";import{R as C}from"./index-BMVQvedj.js";import{D as d}from"./data-editor-all-ZqeXZrI7.js";import{B as g,D as u,P as o,a as f,d as A}from"./utils-Eaa2yR9r.js";import{S as h}from"./story-utils-K2EZnGjM.js";import"./iframe-CHucUuiQ.js";import"../sb-preview/runtime.js";import"./image-window-loader-KuvgVtpW.js";import"./throttle-7EuXLZa7.js";import"./_baseIteratee-WTHxv43n.js";import"./flatten-qRvRBp6y.js";import"./scrolling-data-grid-Lq0bpFDr.js";import"./index-PWBWJyi_.js";import"./index.esm-Ejw8GwRl.js";import"./index-wocATsGp.js";const W={title:"Glide-Data-Grid/DataEditor Demos",decorators:[l=>t(h,{children:t(g,{title:"Content Alignment",description:p(u,{children:["You can customize the content alignment by setting ",t(o,{children:"contentAlign"})," of a cell to ",t(o,{children:"left"}),", ",t(o,{children:"right"})," or ",t(o,{children:"center"}),"."]}),children:t(l,{})})})]},r=()=>{const{cols:l,getCellContent:n}=f(),m=C.useCallback(e=>{const[i,D]=e;return i===3?{...n(e),contentAlign:"center"}:i===4?{...n(e),contentAlign:"right"}:i===5?{...n(e),contentAlign:"left"}:n(e)},[n]);return t(d,{...A,getCellContent:m,columns:l,rows:300})};var c,s,a;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`() => {
+  const {
+    cols,
+    getCellContent
+  } = useAllMockedKinds();
+  const mangledGetCellContent = React.useCallback<typeof getCellContent>(cell => {
+    const [col, _row] = cell;
+    if (col === 3) {
+      return {
+        ...getCellContent(cell),
+        contentAlign: "center"
+      };
+    }
+    if (col === 4) {
+      return {
+        ...getCellContent(cell),
+        contentAlign: "right"
+      };
+    }
+    if (col === 5) {
+      return {
+        ...getCellContent(cell),
+        contentAlign: "left"
+      };
+    }
+    return getCellContent(cell);
+  }, [getCellContent]);
+  return <DataEditor {...defaultProps} getCellContent={mangledGetCellContent} columns={cols} rows={300} />;
+}`,...(a=(s=r.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};const z=["ContentAlignment"];export{r as ContentAlignment,z as __namedExportsOrder,W as default};
---
+++ docs/assets/obscured-grid.stories-EMKHbtaf.js
@@ -0,0 +1,15 @@
+import{j as o,a as n,F as c}from"./marked.esm-dbrxtycE.js";import"./index-BMVQvedj.js";import{D as u}from"./data-editor-all-ZqeXZrI7.js";import{B as C,D as m,M as p,u as g,d as x}from"./utils-Eaa2yR9r.js";import{S as h}from"./story-utils-K2EZnGjM.js";import"./iframe-CHucUuiQ.js";import"../sb-preview/runtime.js";import"./image-window-loader-KuvgVtpW.js";import"./throttle-7EuXLZa7.js";import"./_baseIteratee-WTHxv43n.js";import"./flatten-qRvRBp6y.js";import"./scrolling-data-grid-Lq0bpFDr.js";import"./index-PWBWJyi_.js";import"./index.esm-Ejw8GwRl.js";import"./index-wocATsGp.js";const S={title:"Glide-Data-Grid/DataEditor Demos",decorators:[r=>o(h,{children:n(C,{title:"Obscured Data Grid",description:n(c,{children:[o(m,{children:"The data grid should respect being obscured by other elements"}),o(p,{children:"This is mostly a test area because its hard to test with unit tests."})]}),children:[o(r,{}),o("div",{style:{position:"absolute",top:0,left:"50%",width:"50%",height:"100%",background:"rgba(0,0,0,0.5)",zIndex:100}})]})})]},t=()=>{const{cols:r,getCellContent:i,setCellValue:d}=g(60,!1);return o(u,{...x,getCellContent:i,onItemHovered:e=>console.log("onItemHovered",e),onCellClicked:e=>console.log("onCellClicked",e),onHeaderClicked:e=>console.log("onHeaderClicked",e),onCellContextMenu:e=>console.log("onCellContextMenu",e),onHeaderContextMenu:e=>console.log("onHeaderContextMenu",e),columns:r,rowMarkers:"both",onPaste:!0,onCellEdited:d,trailingRowOptions:{sticky:!0,tint:!0,hint:"New row..."},rows:1e4})};var l,s,a;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`() => {
+  const {
+    cols,
+    getCellContent,
+    setCellValue
+  } = useMockDataGenerator(60, false);
+  return <DataEditor {...defaultProps} getCellContent={getCellContent} onItemHovered={x => console.log("onItemHovered", x)} onCellClicked={x => console.log("onCellClicked", x)} onHeaderClicked={x => console.log("onHeaderClicked", x)} onCellContextMenu={x => console.log("onCellContextMenu", x)} onHeaderContextMenu={x => console.log("onHeaderContextMenu", x)} columns={cols} rowMarkers={"both"} onPaste={true} // we want to allow paste to just call onCellEdited
+  onCellEdited={setCellValue} // Sets the mock cell content
+  trailingRowOptions={{
+    // How to get the trailing row to look right
+    sticky: true,
+    tint: true,
+    hint: "New row..."
+  }} rows={10_000} />;
+}`,...(a=(s=t.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};const _=["ObscuredDataGrid"];export{t as ObscuredDataGrid,_ as __namedExportsOrder,S as default};
