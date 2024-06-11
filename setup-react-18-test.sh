0ac1a8143922ddb8332cc03b287714518f03175b
BrianHung <brianlhung@gmail.com>
2024-06-11 02:19:17 -0700

    fix react 18 test

--- setup-react-18-test.sh
+++ setup-react-18-test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-npm i -D react@latest react-dom@latest @testing-library/react@latest @testing-library/react-hooks@latest @testing-library/user-event@14.5.1 react-test-renderer@latest
+npm i -D react@latest react-dom@latest @testing-library/react@latest @testing-library/react-hooks@latest @testing-library/user-event@14.5.1 react-test-renderer@latest @testing-library/dom
