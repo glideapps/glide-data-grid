a7b47d82e4d1c535b28cbf226949319f49671130
Brian Hung <brianlhung@gmail.com>
2025-06-25 08:16:07 -0700

    Add header disabled to row markers (#981)
    * add header disabled to row markers
    
    * disable pointer if row header disabled
    
    * remove console .log
    
    * Add toggle to story
    
    ---------
    
    Co-authored-by: lukasmasuch <lukas.masuch@gmail.com>

--- packages/core/src/internal/data-grid/data-grid.tsx
+++ packages/core/src/internal/data-grid/data-grid.tsx
@@ -924,7 +924,7 @@ const DataGrid: React.ForwardRefRenderFunction<DataGridRef, DataGridProps> = (p,
     const [overFill, setOverFill] = React.useState(false);
 
     const [hCol, hRow] = hoveredItem ?? [];
-    const headerHovered = hCol !== undefined && hRow === -1;
+    const headerHovered = hCol !== undefined && hRow === -1 && columns[hCol].headerRowMarkerDisabled !== true;
     const groupHeaderHovered = hCol !== undefined && hRow === -2;
     let clickableInnerCellHovered = false;
     let editableBoolHovered = false;
@@ -949,6 +949,7 @@ const DataGrid: React.ForwardRefRenderFunction<DataGridRef, DataGridProps> = (p,
         : headerHovered || clickableInnerCellHovered || editableBoolHovered || groupHeaderHovered
         ? "pointer"
         : "default";
+    
     const style = React.useMemo(
         () => ({
             // width,
